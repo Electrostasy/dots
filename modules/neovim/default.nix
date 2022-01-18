126bd612adb26cf5e745b44235394066f1723760
Gediminas Valys <steamykins@gmail.com>
2022-01-18 10:52:16 +0200

    nvim: install all treesitter grammars

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -4,33 +4,7 @@
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
-      (nvim-treesitter.withPlugins (
-        grammar: with grammar; [
-          tree-sitter-bash
-          tree-sitter-c
-          tree-sitter-clojure
-          tree-sitter-cmake
-          tree-sitter-commonlisp
-          tree-sitter-cpp
-          tree-sitter-css
-          tree-sitter-fish
-          tree-sitter-html
-          tree-sitter-json
-          tree-sitter-latex
-          tree-sitter-lua
-          tree-sitter-make
-          tree-sitter-markdown
-          tree-sitter-nix
-          tree-sitter-python
-          tree-sitter-query
-          tree-sitter-rst
-          tree-sitter-rust
-          tree-sitter-scss
-          tree-sitter-toml
-          tree-sitter-vim
-          tree-sitter-yaml
-        ]
-      )) # tree-sitter code AST
+      (nvim-treesitter.withPlugins(p: builtins.attrValues p)) # tree-sitter code AST
       cmp-buffer # nvim-cmp completion buffer source
       cmp-cmdline # nvim-cmp commands completion source
       cmp-nvim-lsp # nvim-cmp completion LSP source
@@ -65,13 +39,13 @@
       nixpkgs-fmt # Nix code formatter
       nodePackages.bash-language-server
       nodePackages.pyright # Python LSP
-      python3Packages.black # Python code formatter
       rnix-lsp # Nix LSP
       statix # Nix code linter
       sumneko-lua-language-server # Lua LSP
       texlab # LaTeX LSP
-      tree-sitter # Incremental language parsing
+      tree-sitter # Incremental parser
       valgrind # Memory debugging
+      python3Packages.black # Python code formatter
     ];
     extraConfig = ''
       " Home-Manager and NixOS currently do not support a pure Lua config
