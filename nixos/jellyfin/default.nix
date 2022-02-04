cfd334db043a1432d8515e030ea3f26b649213aa
Gediminas Valys <steamykins@gmail.com>
2022-02-04 10:58:18 +0200

    nixos: add initial jellyfin module

---
+++ nixos/jellyfin/default.nix
@@ -0,0 +1,26 @@
+{ config, ... }:
+
+{
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+
+    upstreams."jellyfin".servers = {
+      "127.0.0.1:8096" = {};
+    };
+
+    virtualHosts = {
+      # "media.127.0.0.1" = {
+      "127.0.0.1" = {
+        locations = {
+          "/".proxyPass = "http://jellyfin";
+        };
+      };
+    };
+  };
+}
