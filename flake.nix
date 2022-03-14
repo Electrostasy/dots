a66323b9964284d39ac0ac4041cd3bbcbac5af4f
Gediminas Valys <steamykins@gmail.com>
2022-03-14 17:49:45 +0200

    Revert "nixpkgs: nixpkgs-unstable->nixos-unstable-small"
    This reverts commit 4edd47a5e36036f1212b167db89bd682f0136737.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
