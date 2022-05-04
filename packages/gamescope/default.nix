62c5c9ae2bb8521a76b7315ef1830874d8c6fbda
Gediminas Valys <steamykins@gmail.com>
2022-05-04 18:53:05 +0300

    packages/gamescope: 3.11.28 -> 3.11.31-beta2

--- packages/gamescope/default.nix
+++ packages/gamescope/default.nix
@@ -42,8 +42,8 @@ let
   gamescope-src = fetchFromGitHub {
     owner = "plagman";
     repo = "gamescope";
-    rev = "3.11.28";
-    sha256 = "sha256-hjDOnRr9RwvvcsIbM6Z4Fr+p8l3EK6DYCO6LQuENmGY=";
+    rev = "3.11.31-beta2";
+    sha256 = "sha256-r3eEkyaqOc/uZBNj52d50QT+jYNC/BHwcvqn6ba+ass=";
   };
   libliftoff_2_0_0 = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
@@ -63,7 +63,7 @@ in
 
 stdenv.mkDerivation {
   pname = "gamescope";
-  version = "3.11.28";
+  version = "3.11.31-beta2";
 
   src = gamescope-src;
 
