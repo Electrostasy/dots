d3de48dae626e8709b1e102ca4b6618f35292e31
Gediminas Valys <steamykins@gmail.com>
2022-05-17 21:20:12 +0300

    packages/umc: fix broken url

--- packages/umc/default.nix
+++ packages/umc/default.nix
@@ -1,11 +1,11 @@
-{ fetchurl, stdenv }:
+{ fetchzip, stdenv }:
 
 stdenv.mkDerivation {
   pname = "umc";
   version = "0.2";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/umc/umc-0.2/umc-0.2.tar.gz";
-    sha256 = "sha256-QgFn2DRRNnw5Y106nVwj9lwnk7QQ8E8Fa6NHQJgmSz4=";
+  src = fetchzip {
+    url = "mirror://sourceforge/umc/umc/umc-0.2/umc-0.2.tar.gz";
+    sha256 = "sha256-N4AVvViMgZAmxS5EI9fqaxeKFZK7Qa+rfd1h4NqMFrE=";
   };
 }
