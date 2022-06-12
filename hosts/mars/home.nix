dcc7205103464bc2a86a56e51ffe38eb1cad7b35
Gediminas Valys <steamykins@gmail.com>
2022-06-12 11:08:34 +0300

    hosts/mars: add `/etc/nixos` to git safe.directory

--- hosts/mars/home.nix
+++ hosts/mars/home.nix
@@ -202,6 +202,10 @@
       enable = true;
       userName = "Gediminas Valys";
       userEmail = "steamykins@gmail.com";
+      extraConfig = {
+        # https://github.com/NixOS/nixpkgs/issues/169193#issuecomment-1103816735
+        safe.directory = "/etc/nixos";
+      };
     };
   };
 }
