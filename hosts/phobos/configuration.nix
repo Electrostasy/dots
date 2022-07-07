9ec98c89d824bcc76e86bd630aa087d3f7a951e4
Gediminas Valys <steamykins@gmail.com>
2022-07-07 22:55:35 +0300

    hosts/phobos: merge {hardware-configuration,configuration}.nix

--- hosts/phobos/configuration.nix
+++ hosts/phobos/configuration.nix
@@ -1,25 +1,64 @@
-{ config, pkgs, ... }:
+{ config, pkgs, persistMount, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ./media.nix
-  ];
+  imports = [ ./media.nix ];
 
   system.stateVersion = "22.05";
 
   boot = {
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
-    kernelPackages = pkgs.linuxPackages_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "uas" ];
     kernelParams = [
       "8250.nr_uarts=1"
       "console=ttyAMA0,115200"
       "console=tty1"
       "cma=128M"
     ];
+    kernelPackages = pkgs.linuxPackages_rpi4;
+    tmpOnTmpfs = true;
+
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
+
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = [ "defaults" "size=256M" "mode=755" ];
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+    "/nix" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "noatime" "nodiratime" "compress-force=zstd:3" ];
+    };
+
+    "/state" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=state" "noatime" "nodiratime" "compress-force=zstd:3" ];
+      neededForBoot = true;
+    };
+  };
+
+  environment.persistence.${persistMount} = {
+    directories = [
+      "/etc/nixos"
+      "/etc/ssh"
+      "/var/log"
+    ];
+    files = [ "/etc/machine-id" ];
   };
 
   time.timeZone = "Europe/Vilnius";
