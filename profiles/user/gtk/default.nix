5a36f61d04c4d912ae02757682bb5aae91e9cf8c
Gediminas Valys <steamykins@gmail.com>
2022-07-30 18:53:50 +0300

    profiles/gtk: prefer dark theme in gtk4

--- profiles/user/gtk/default.nix
+++ profiles/user/gtk/default.nix
@@ -9,6 +9,10 @@
       name = "Adwaita-dark";
     };
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+    gtk4.extraConfig = {
+      # NOTE: doesn't work with libadwaita programs
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
   home.pointerCursor.gtk.enable = true;
 }
