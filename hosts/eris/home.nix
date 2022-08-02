7e1ffef0e9fa4a205ea3464fbe9ad2ca928b13f1
Gediminas Valys <steamykins@gmail.com>
2022-08-02 08:20:48 +0000

    hosts/eris: add more pkgs

--- hosts/eris/home.nix
+++ hosts/eris/home.nix
@@ -4,39 +4,37 @@
   home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
+    bintools-unwrapped
+    binwalk
+    dos2unix
+    exiftool
     ffmpeg
+    file
     hashcat
     imagemagick
     john
     jq
     libewf
-    qemu
     ripgrep
+    stegseek
     tealdeer
+    unixtools.xxd
+    wget
     xplr
+    xsv
   ];
 
-  programs = {
-    zellij = {
-      enable = true;
+  programs.lsd = {
+    enable = true;
 
-      settings = {
-
-      };
-    };
-
-    lsd = {
-      enable = true;
-
-      settings = {
-        classic = false;
-        blocks = [ "permission" "user" "group" "size" "date" "name" ];
-        date = "+%Y-%m-%d %H:%M:%S %z";
-        dereference = true;
-        sorting = {
-          column = "name";
-          dir-grouping = "first";
-        };
+    settings = {
+      classic = false;
+      blocks = [ "permission" "user" "group" "size" "date" "name" ];
+      date = "+%Y-%m-%d %H:%M:%S %z";
+      dereference = true;
+      sorting = {
+        column = "name";
+        dir-grouping = "first";
       };
     };
   };
