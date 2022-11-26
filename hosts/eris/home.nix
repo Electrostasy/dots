e707e22beb2da8c3a5f607de96d950de492f0dcb
Gediminas Valys <steamykins@gmail.com>
2022-11-26 16:01:25 +0200

    hosts/eris: add bgrep, sleuthkit, testdisk pkgs

--- hosts/eris/home.nix
+++ hosts/eris/home.nix
@@ -4,6 +4,7 @@
   home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
+    bgrep
     bintools-unwrapped
     binwalk
     dos2unix
@@ -16,7 +17,9 @@
     jq
     libewf
     ripgrep
+    sleuthkit # mmls, fls, fsstat, icat
     stegseek
+    testdisk # photorec
     unixtools.xxd
     wget
     xplr
