c1ad1f1be1f1c45056d84ea0dcbd1ad9c98bdb6a
Gediminas Valys <steamykins@gmail.com>
2022-12-31 17:44:35 +0200

    profiles/audio: disable midi support

--- profiles/system/audio/default.nix
+++ profiles/system/audio/default.nix
@@ -11,4 +11,11 @@
     pulse.enable = true;
     alsa.enable = true;
   };
+
+  environment.etc."wireplumber/main.lua.d/51-midi.lua".text = ''
+    alsa_monitor.properties = {
+      ["alsa.midi"] = false,
+      ["alsa.midi.monitoring"] = false,
+    }
+  '';
 }
