d4070c7540544ba007c0689bb519b71489b0dc9e
Gediminas Valys <steamykins@gmail.com>
2023-03-10 22:53:05 +0200

    profiles/wayfire: re-enable winshadows plugin

--- profiles/user/wayfire/default.nix
+++ profiles/user/wayfire/default.nix
@@ -82,6 +82,7 @@
   home.packages = with pkgs; [
     wf-recorder
     wl-clipboard
+    xdg-utils
   ];
 
   wayland.windowManager.wayfire = {
@@ -113,18 +114,16 @@
           };
         }
 
-        # Can't compile until changes in wayfire-git are caught up with, turn
-        # off for now.
-        # { package = pkgs.wayfirePlugins.shadows;
-        #   plugin = "winshadows";
-        #   settings = {
-        #     include_undecorated_views = true;
-        #     shadow_color = "\\#000000FF";
-        #     shadow_radius = 20;
-        #     glow_color = "\\#2D4F6733";
-        #     glow_radius = 40;
-        #   };
-        # }
+        { package = pkgs.wayfirePlugins.shadows;
+          plugin = "winshadows";
+          settings = {
+            include_undecorated_views = true;
+            shadow_color = "\\#00000033";
+            shadow_radius = 20;
+            glow_color = "\\#97AFCD26";
+            glow_radius = 40;
+          };
+        }
         { plugin = "decoration";
           settings = {
             active_color = "\\#2D4F67FF";
