03c4e35eeed5821b6e5a63434b819f6141cd2d0c
Gediminas Valys <steamykins@gmail.com>
2023-03-31 12:41:28 +0300

    hosts/terra: refactor wormhole-rs alias

--- hosts/terra/home.nix
+++ hosts/terra/home.nix
@@ -124,12 +124,8 @@
       settings.flags.tree = true;
     };
 
-    # Add fish shell completion for "wormhole" alias.
-    programs.fish = {
-      shellAliases.wormhole = "wormhole-rs";
-      interactiveShellInit = ''
-        complete --command wormhole --wraps wormhole-rs
-      '';
+    programs.fish.shellAliases = {
+      wormhole = "wormhole-rs";
     };
 
     xdg.configFile."weathercrab/wthrr.ron".text = ''
