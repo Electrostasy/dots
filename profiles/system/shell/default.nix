e62f01e504e3ce6d15116e549636dfe656f98d14
Gediminas Valys <steamykins@gmail.com>
2023-06-28 12:22:54 +0300

    profiles/shell: add cmd duration to right prompt

--- profiles/system/shell/default.nix
+++ profiles/system/shell/default.nix
@@ -28,6 +28,10 @@
       # Prompt configuration.
       function fish_right_prompt -d "Print the right-side prompt"
         set_color $fish_color_autosuggestion; date '+%H:%M:%S'
+        if test $CMD_DURATION && test $CMD_DURATION -ne 0
+          echo " "
+          set_color $fish_color_quote; echo "$(math "$CMD_DURATION/1000")s"
+        end
       end
 
       # Custom command-not-found handler using nix-index and syntax highlights.
