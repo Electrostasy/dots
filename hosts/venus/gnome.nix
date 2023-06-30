cb5ee8dd44904093c7e10239360adb152c6a02f5
Gediminas Valys <steamykins@gmail.com>
2023-06-30 23:57:31 +0300

    hosts/venus: update gnome config

--- hosts/venus/gnome.nix
+++ hosts/venus/gnome.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
 {
-  # TODO: Investigate using seahorse ssh-askpass or similar for password prompts.
-  programs.ssh.enableAskPassword = false;
+  # Required for ssh-askpass.
+  programs.seahorse.enable = true;
 
   # Due to the way desktop configuration works in Nixpkgs, we have to install
   # an X server even if we only use Wayland.
@@ -31,54 +31,78 @@
   };
 
   # Disable default GNOME module features.
-  services.avahi.enable = false;
-  services.gnome = {
-    core-utilities.enable = false;
-    tracker-miners.enable = false;
-    tracker.enable = false;
+  hardware.pulseaudio.enable = false;
+  services = {
+    avahi.enable = false;
+
+    gnome = {
+      core-utilities.enable = false;
+      tracker-miners.enable = false;
+      tracker.enable = false;
+    };
+
+    pipewire = {
+      enable = true;
+
+      pulse.enable = true;
+      alsa.enable = true;
+    };
   };
-  environment.gnome.excludePackages = with pkgs.gnome; [
-    geary
-    gnome-bluetooth
-    gnome-terminal
-    pkgs.gnome-tour
-    pkgs.orca
-  ];
 
   # NOTE: Currently cannot connect via GUI, has to be done via iwctl.
   networking.networkmanager.wifi.backend = "iwd";
 
-  # Persist dconf settings.
-  # TODO: Manage dconf with Nix.
-  environment.persistence."/state".users.electro.directories = [
-    ".config/dconf"
-    ".config/burn-my-windows"
-  ];
+  environment = {
+    gnome.excludePackages = with pkgs.gnome; [
+      geary
+      gnome-bluetooth
+      gnome-terminal
+      pkgs.gnome-tour
+      pkgs.orca
+    ];
 
-  environment.systemPackages = with pkgs; [
-    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-    recursive
+    persistence."/state".users.electro = {
+      # Persists multi-monitor configuration.
+      files = [ ".config/monitors.xml" ];
 
-    blackbox-terminal
-    celluloid
-    eyedropper
-    gnome.gnome-calculator
-    gnome.gnome-calendar
-    gnome.gnome-system-monitor
-    gnome.gnome-weather
-    gnome.nautilus
-    gnome.sushi
-    junction
-    # newsflash
-    rnote
-    warp
-  ] ++ (with pkgs.gnomeExtensions; [
-    blur-my-shell
-    burn-my-windows
-    dash-to-panel
-    date-menu-formatter
-    desktop-cube
-    pano
-    screen-rotate
-  ]);
+      # TODO: Manage dconf with Nix.
+      directories = [
+        ".config/burn-my-windows"
+        ".config/dconf"
+      ];
+    };
+
+    sessionVariables.GTK_THEME = "adw-gtk3-dark";
+
+    systemPackages = with pkgs; [
+      adw-gtk3
+
+      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+      recursive
+
+      amberol
+      blackbox-terminal
+      celluloid
+      eyedropper
+      fractal-next
+      gnome.cheese
+      gnome.gnome-calculator
+      gnome.gnome-calendar
+      gnome.gnome-system-monitor
+      gnome.gnome-weather
+      gnome.nautilus
+      gnome.sushi
+      rnote
+      video-trimmer
+      warp
+    ] ++ (with pkgs.gnomeExtensions; [
+      blur-my-shell
+      burn-my-windows
+      dash-to-panel
+      date-menu-formatter
+      desktop-cube
+      pano
+      screen-rotate
+    ]);
+  };
 }
