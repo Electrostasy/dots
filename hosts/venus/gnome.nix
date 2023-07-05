977c528d2cf869a1add92903a7f68c87233fb123
Gediminas Valys <steamykins@gmail.com>
2023-07-05 13:21:32 +0300

    hosts/venus: fix GNOME session crash on login

--- hosts/venus/gnome.nix
+++ hosts/venus/gnome.nix
@@ -24,9 +24,16 @@
     ]);
 
     desktopManager.gnome.enable = true;
-    displayManager.gdm = {
-      enable = true;
-      autoSuspend = false;
+    displayManager = {
+      # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+      # Suggested workaround (disabling "getty@tty1" and "autovt@tty1"
+      # works, but as I do not use autoLogin anyway, this should suffice.
+      autoLogin.enable = false;
+
+      gdm = {
+        enable = true;
+        autoSuspend = false;
+      };
     };
   };
 
