0498c468ef47b923c00d24f99ffc1f1a258073d1
Gediminas Valys <steamykins@gmail.com>
2023-12-31 01:04:05 +0200

    profiles/gnome: separate shell extensions

--- profiles/system/gnome/default.nix
+++ profiles/system/gnome/default.nix
@@ -1,32 +1,8 @@
 { pkgs, lib, ... }:
 
-let
-  gnomeShellExtensions = with pkgs.gnomeExtensions; [
-    blur-my-shell
-    burn-my-windows
-    dash-to-panel
-    desktop-cube
-    native-window-placement
-    panel-date-format
-    tophat
-  ];
-
-  burnMyWindowsProfile = pkgs.writeText "nix-profile.conf" ''
-    [burn-my-windows-profile]
-
-    profile-high-priority=true
-    profile-window-type=0
-    profile-animation-type=0
-    fire-enable-effect=false
-    glide-enable-effect=true
-    glide-animation-time=250
-    glide-squish=0.0
-    glide-tilt=0.0
-    glide-shift=0.0
-    glide-scale=0.85
-  '';
-in
 {
+  imports = [ ./extensions.nix ];
+
   # Due to the way desktop configuration works in Nixpkgs, we have to install
   # an X server even if we only use Wayland.
   services.xserver = {
@@ -101,7 +77,7 @@ in
       pkgs.orca # screen reader
       simple-scan
       totem # video player
-      yelp# help viewer
+      yelp # help viewer
     ];
 
     # TODO: Make independent of user.
@@ -115,9 +91,7 @@ in
 
     sessionVariables.GTK_THEME = "adw-gtk3-dark";
 
-    variables.GI_TYPELIB_PATH = "/run/current-system/sw/lib/girepository-1.0";
-
-    systemPackages = with pkgs; gnomeShellExtensions ++ [
+    systemPackages = with pkgs; [
       adw-gtk3
 
       (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
@@ -128,206 +102,141 @@ in
       nautilus-amberol
       nautilus-vimv
 
-      # Needed for gnomeExtensions.tophat + GI_TYPELIB_PATH as per issue:
-      # https://github.com/fflewddur/tophat/issues/106#issuecomment-1848319826
-      libgtop
-
-      amberol # Music player
-      celluloid # MPV-based video player
-      eyedropper # Colour picker
-      fractal # Matrix client
+      amberol
+      celluloid
+      eyedropper
+      fractal
       keepassxc
-      tagger # Music tagger
-      warp # Magic Wormhole client
+      resources
+      tagger
+      warp
     ];
   };
 
-  programs.dconf.profiles = {
-    # TODO: Investigate customizing gdm greeter.
-    user.databases = [{
-      settings = with lib.gvariant; {
-        "org/gnome/desktop/calendar".show-weekdate = true;
-        "org/gnome/desktop/input-sources".sources = [
-          (mkTuple [ "xkb" "us" ])
-          (mkTuple [ "xkb" "lt" ])
-        ];
+  programs.dconf.profiles.user.databases = [{
+    settings = with lib.gvariant; {
+      "org/gnome/desktop/calendar".show-weekdate = true;
+      "org/gnome/desktop/input-sources".sources = [
+        (mkTuple [ "xkb" "us" ])
+        (mkTuple [ "xkb" "lt" ])
+      ];
 
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-          show-battery-percentage = true;
-          monospace-font-name = "Recursive 11";
-        };
-
-        "org/gnome/desktop/media-handling".automount = false;
-        "org/gnome/desktop/peripherals/mouse".accel-profile = "flat";
-        "org/gnome/desktop/privacy".remember-recent-files = false;
-        "org/gnome/desktop/screensaver".lock-enabled = false;
-        "org/gnome/desktop/session".idle-delay = mkUint32 0;
-        "org/gnome/desktop/wm/preferences".resize-with-right-button = true;
-        "org/gnome/mutter" = {
-          edge-tiling = true;
-          attach-modal-dialogs = true;
-          experimental-features = [ "scale-monitor-framebuffer" ];
-        };
-
-        "org/gnome/settings-daemon/plugins/power" = {
-          # Suspend only on battery power, not while charging.
-          sleep-inactive-ac-type = "nothing";
-          power-button-action = "interactive";
-        };
-
-        "org/gnome/nautilus/preferences".default-folder-viewer = "list-view";
-        "org/gnome/nautilus/list-view" = {
-          use-tree-view = true;
-          default-zoom-level = "small";
-        };
-
-        "org/gtk/gtk4/settings/file-chooser" = {
-          sort-directories-first = true;
-          show-hidden = true;
-          view-type = "list";
-        };
-
-        "io/github/celluloid-player/celluloid" = {
-          always-open-new-window = true;
-          always-autohide-cursor = true;
-          always-show-title-buttons = true;
-          autofit-enable = false;
-          mpv-config-enable = true;
-          mpv-config-file = "file:///home/electro/.config/mpv/mpv.conf";
-          mpv-input-config-enable = true;
-          mpv-input-config-file = "file:///home/electro/.config/mpv/input.conf";
-        };
-
-        # Hidden/background programs only show up if they are flatpaks,
-        # so disable background play for now.
-        "io/bassi/Amberol".background-play = false;
-
-        "org/gnome/settings-daemon/plugins/media-keys" = {
-          screenreader = mkEmptyArray type.string;
-          magnifier = mkEmptyArray type.string;
-          calculator = [ "<Super>c" ];
-        };
-
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-          binding = "<Super>Return";
-          command = "/usr/bin/env kgx";
-          name = "Terminal";
-        };
-
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-          binding = "<Super>e";
-          command = "/usr/bin/env nautilus";
-          name = "File Manager";
-        };
-
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-          binding = "<Super>k";
-          command = "/usr/bin/env keepassxc";
-          name = "Password Manager";
-        };
-
-        "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
-        ];
-
-        "org/gnome/desktop/wm/keybindings" = {
-          switch-to-workspace-left = [ "<Super>a" ];
-          switch-to-workspace-right = [ "<Super>d" ];
-          move-to-workspace-left = [ "<Shift><Super>a" ];
-          move-to-workspace-right = [ "<Shift><Super>d" ];
-          switch-to-workspace-1 = [ "<Super>1" ];
-          switch-to-workspace-2 = [ "<Super>2" ];
-          switch-to-workspace-3 = [ "<Super>3" ];
-          switch-to-workspace-4 = [ "<Super>4" ];
-          # https://unix.stackexchange.com/a/436347
-          switch-input-source = [ "<Alt>Shift_L" ];
-          switch-input-source-backward = mkEmptyArray type.string;
-          activate-window-menu = [ "Menu" ];
-          close = [ "<Shift><Super>w" ];
-          maximize = [ "<Super>f" ];
-          toggle-fullscreen = [ "<Shift><Super>f" ];
-        };
-
-        "org/gnome/shell/keybindings" = {
-          # Following binds are replaced by the ones above.
-          toggle-application-view = mkEmptyArray type.string;
-          switch-to-application-1 = mkEmptyArray type.string;
-          switch-to-application-2 = mkEmptyArray type.string;
-          switch-to-application-3 = mkEmptyArray type.string;
-          switch-to-application-4 = mkEmptyArray type.string;
-
-          show-screen-recording-ui = mkEmptyArray type.string;
-          screenshot = mkEmptyArray type.string;
-          show-screenshot-ui = [ "<Shift><Super>s" ];
-          screenshot-window = mkEmptyArray type.string;
-        };
-
-        "org/gnome/shell".enabled-extensions =
-          builtins.map
-            (x: x.extensionUuid)
-            gnomeShellExtensions;
-
-        "org/gnome/shell/extensions/panel-date-format".format = "%Y-%m-%d %H:%M";
-
-        "org/gnome/shell/extensions/dash-to-panel" = {
-          # Even when we are not using multiple panels on multiple monitors,
-          # the extension still creates them in the config, so we set the same
-          # configuration for each (up to 2 monitors).
-          panel-positions = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: "TOP"));
-          panel-sizes = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: 32));
-          panel-element-positions = builtins.toJSON (lib.genAttrs [ "0" "1" ] (x: [
-            { element = "showAppsButton"; visible = false; position = "stackedTL"; }
-            { element = "activitiesButton"; visible = true; position = "stackedTL"; }
-            { element = "dateMenu"; visible = true; position = "stackedTL"; }
-            { element = "rightBox"; visible = true; position = "stackedTL"; }
-            { element = "taskbar"; visible = true; position = "centerMonitor"; }
-            { element = "leftBox"; visible = true; position = "stackedBR"; }
-            { element = "centerBox"; visible = false; position = "centered"; }
-            { element = "systemMenu"; visible = true; position = "stackedBR"; }
-            { element = "desktopButton"; visible = false; position = "stackedBR"; }
-          ]));
-          multi-monitors = false;
-          focus-highlight-dominant = true;
-          dot-size = mkInt32 2;
-          dot-position = "TOP";
-          dot-color-dominant = true;
-          appicon-padding = mkInt32 2;
-          appicon-margin = mkInt32 2;
-          trans-use-custom-opacity = true;
-          trans-panel-opacity = 0.25;
-          show-favorites = false;
-          group-apps = false;
-          isolate-workspaces = true;
-          hide-overview-on-startup = true;
-          stockgs-keep-dash = true;
-        };
-        "org/gnome/shell/extensions/blur-my-shell".color-and-noise = false;
-        "org/gnome/shell/extensions/blur-my-shell/applications".blur = false;
-        "org/gnome/shell/extensions/burn-my-windows".active-profile = "${burnMyWindowsProfile}";
-        "org/gnome/shell/extensions/blur-my-shell/panel".override-background = false;
-        "org/gnome/shell/extensions/desktop-cube" = {
-          last-first-gap = false;
-          window-parallax = 0.75;
-          edge-switch-pressure = mkUint32 100;
-          mouse-rotation-speed = 1.0;
-        };
-
-        "org/gnome/shell/extensions/tophat" = {
-          cpu-display = "both";
-          mem-display = "both";
-          show-disk = false;
-        };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        show-battery-percentage = true;
+        monospace-font-name = "Recursive 11";
       };
-    }];
-  };
+
+      "org/gnome/desktop/media-handling".automount = false;
+      "org/gnome/desktop/peripherals/mouse".accel-profile = "flat";
+      "org/gnome/desktop/privacy".remember-recent-files = false;
+      "org/gnome/desktop/screensaver".lock-enabled = false;
+      "org/gnome/desktop/session".idle-delay = mkUint32 0;
+      "org/gnome/desktop/wm/preferences".resize-with-right-button = true;
+      "org/gnome/mutter" = {
+        edge-tiling = true;
+        attach-modal-dialogs = true;
+        experimental-features = [ "scale-monitor-framebuffer" ];
+      };
+
+      "org/gnome/settings-daemon/plugins/power" = {
+        # Suspend only on battery power, not while charging.
+        sleep-inactive-ac-type = "nothing";
+        power-button-action = "interactive";
+      };
+
+      "org/gnome/nautilus/preferences".default-folder-viewer = "list-view";
+      "org/gnome/nautilus/list-view" = {
+        use-tree-view = true;
+        default-zoom-level = "small";
+      };
+
+      "org/gtk/gtk4/settings/file-chooser" = {
+        sort-directories-first = true;
+        show-hidden = true;
+        view-type = "list";
+      };
+
+      "io/github/celluloid-player/celluloid" = {
+        always-open-new-window = true;
+        always-autohide-cursor = true;
+        always-show-title-buttons = true;
+        autofit-enable = false;
+        mpv-config-enable = true;
+        mpv-config-file = "file:///home/electro/.config/mpv/mpv.conf";
+        mpv-input-config-enable = true;
+        mpv-input-config-file = "file:///home/electro/.config/mpv/input.conf";
+      };
+
+      # Hidden/background programs only show up if they are flatpaks,
+      # so disable background play for now.
+      "io/bassi/Amberol".background-play = false;
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        screenreader = mkEmptyArray type.string;
+        magnifier = mkEmptyArray type.string;
+        calculator = [ "<Super>c" ];
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Super>Return";
+        command = "/usr/bin/env kgx";
+        name = "Terminal";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        binding = "<Super>e";
+        command = "/usr/bin/env nautilus";
+        name = "File Manager";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+        binding = "<Super>k";
+        command = "/usr/bin/env keepassxc";
+        name = "Password Manager";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+      ];
+
+      "org/gnome/desktop/wm/keybindings" = {
+        switch-to-workspace-left = [ "<Super>a" ];
+        switch-to-workspace-right = [ "<Super>d" ];
+        move-to-workspace-left = [ "<Shift><Super>a" ];
+        move-to-workspace-right = [ "<Shift><Super>d" ];
+        switch-to-workspace-1 = [ "<Super>1" ];
+        switch-to-workspace-2 = [ "<Super>2" ];
+        switch-to-workspace-3 = [ "<Super>3" ];
+        switch-to-workspace-4 = [ "<Super>4" ];
+        # https://unix.stackexchange.com/a/436347
+        switch-input-source = [ "<Alt>Shift_L" ];
+        switch-input-source-backward = mkEmptyArray type.string;
+        activate-window-menu = [ "Menu" ];
+        close = [ "<Shift><Super>w" ];
+        maximize = [ "<Super>f" ];
+        toggle-fullscreen = [ "<Shift><Super>f" ];
+      };
+
+      "org/gnome/shell/keybindings" = {
+        # Following binds are replaced by the ones above.
+        toggle-application-view = mkEmptyArray type.string;
+        switch-to-application-1 = mkEmptyArray type.string;
+        switch-to-application-2 = mkEmptyArray type.string;
+        switch-to-application-3 = mkEmptyArray type.string;
+        switch-to-application-4 = mkEmptyArray type.string;
+
+        show-screen-recording-ui = mkEmptyArray type.string;
+        screenshot = mkEmptyArray type.string;
+        show-screenshot-ui = [ "<Shift><Super>s" ];
+        screenshot-window = mkEmptyArray type.string;
+      };
+    };
+  }];
 
   systemd.user.tmpfiles.rules = [
-    # Set up `Burn My Windows` config, as it uses a separate file in $HOME/.config.
-    "L+ %h/.config/burn-my-windows/profiles/nix-profile.conf 0755 - - - ${burnMyWindowsProfile}"
-
     # Automatically pick a random wallpaper at startup.
     "L+ %h/.config/autostart/wallpaper.desktop 0755 - - - ${pkgs.writeText "wallpaper.desktop" ''
       [Desktop Entry]
