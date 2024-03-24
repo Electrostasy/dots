42fab07ac11a98fd3734f67b400ea834b2773784
Gediminas Valys <steamykins@gmail.com>
2024-03-24 19:22:35 +0200

    profiles/gnome: fix touchscreen gestures with desktop cube

--- profiles/system/gnome/extensions.nix
+++ profiles/system/gnome/extensions.nix
@@ -71,6 +71,13 @@ in
         window-parallax = 0.75;
         edge-switch-pressure = mkUint32 100;
         mouse-rotation-speed = 1.0;
+
+        # These settings interfere with touchscreen gestures (specifically
+        # 3 finger horizontal swipes for switching workspaces on the desktop
+        # and 3 finger vertical swipes for overview). We can still use the panel.
+        enable-desktop-dragging = false;
+        enable-overview-dragging = false;
+        enable-panel-dragging = true;
       };
 
       "org/gnome/shell/extensions/tiling-assistant" = {
