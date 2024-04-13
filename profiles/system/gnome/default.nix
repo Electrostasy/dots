db84e69fd1ad4c802b8056998768e1ae56a9b263
Gediminas Valys <steamykins@gmail.com>
2024-04-13 18:31:01 +0300

    flake: update lockfile

--- profiles/system/gnome/default.nix
+++ profiles/system/gnome/default.nix
@@ -9,16 +9,9 @@
 
   services.xserver = {
     desktopManager.gnome.enable = true;
-    displayManager = {
-      # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-      # Suggested workaround (disabling "getty@tty1" and "autovt@tty1"
-      # works, but as I do not use autoLogin anyway, this should suffice.
-      autoLogin.enable = false;
-
-      gdm = {
-        enable = true;
-        autoSuspend = false;
-      };
+    displayManager.gdm = {
+      enable = true;
+      autoSuspend = false;
     };
   };
 
