a1f04348b26bf58a56c642d78590007702723e88
Gediminas Valys <steamykins@gmail.com>
2024-07-06 22:18:49 +0300

    profiles/neovim: change name in .desktop file

--- profiles/neovim/default.nix
+++ profiles/neovim/default.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  systemd.tmpfiles.settings."10-neovim"."/home/electro/.config/nvim"."L+".argument = "${./nvim}";
+  systemd.tmpfiles.settings."10-neovim" = {
+    # Link the Lua config for Neovim.
+    "/home/electro/.config/nvim"."L+".argument = "${./nvim}";
+
+    # Override the Neovim wrapper's .desktop file with our own less weird name.
+    "/home/electro/.local/share/applications/nvim.desktop"."L+".argument =
+      let
+        drv = pkgs.runCommand "modify-nvim-desktop" {} ''
+          mkdir -p $out
+          substitute ${config.programs.neovim.finalPackage}/share/applications/nvim.desktop $out/nvim.desktop \
+            --replace-warn 'Name=Neovim wrapper' 'Name=Neovim'
+        '';
+      in "${drv}/nvim.desktop";
+  };
 
   environment.variables = {
     EDITOR = "nvim";
