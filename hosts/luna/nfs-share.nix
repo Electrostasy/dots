599284cd6380e934d87175751daf8243fdb0e88a
Gediminas Valys <steamykins@gmail.com>
2024-07-06 18:11:34 +0300

    hosts/luna: add nfs share mount options

--- hosts/luna/nfs-share.nix
+++ hosts/luna/nfs-share.nix
@@ -6,12 +6,26 @@
     fsType = "nfs4";
     options = [
       "noatime"
-      "nofail"
+
+      # Do not mount automatically at boot, as this can make boot take much longer
+      # if the share is unavailable - mount on demand instead.
+      "noauto"
       "x-systemd.automount"
-      "x-gvfs-show"
+      "x-systemd.mount-timeout=10"
+      "x-systemd.idle-timeout=10min"
+      "_netdev"
+
+      # Default device timeout is 90s, only `nofail` will make boot/shutdown take
+      # 90s longer unless we reconfigure the timeout.
+      "nofail"
+      "x-systemd.device-timeout=5"
+
+      # Set gvfs options for showing up in the file manager with the correct icons
+      # and name set.
       "x-gvfs-icon=folder-remote"
-      "x-gvfs-symbolic-icon=folder-remote-symbolic"
       "x-gvfs-name=${lib.escapeURL "Luna NAS"}"
+      "x-gvfs-show"
+      "x-gvfs-symbolic-icon=folder-remote-symbolic"
     ];
   };
 
