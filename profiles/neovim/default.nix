393488ace4bfac668f98c2559ce1a04e60f88cf8
Gediminas Valys <steamykins@gmail.com>
2024-07-13 10:39:27 +0300

    profiles/neovim: fix tmpfiles unsafe path transition

--- profiles/neovim/default.nix
+++ profiles/neovim/default.nix
@@ -1,11 +1,26 @@
 { config, pkgs, ... }:
 
+let
+  user = config.users.users.electro.name;
+  group = config.users.users.electro.group;
+in
+
 {
   systemd.tmpfiles.settings."10-neovim" = {
+    # Due to how systemd-tmpfiles works, leading directories are implicitly created
+    # if needed, owned by root with an access mode of 0755 by default. In order to
+    # avoid "unsafe path transition" errors, we need to add the appropriate "d" lines.
+    # See tmpfiles.d(5).
+    "/home/electro"."d" = { mode = "0700"; inherit user group; };
+
     # Link the Lua config for Neovim.
+    "/home/electro/.config"."d" = { mode = "0755"; inherit user group; };
     "/home/electro/.config/nvim"."L+".argument = "${./nvim}";
 
     # Override the Neovim wrapper's .desktop file with our own less weird name.
+    "/home/electro/.local"."d" = { mode = "0755"; inherit user group; };
+    "/home/electro/.local/share"."d" = { mode = "0755"; inherit user group; };
+    "/home/electro/.local/share/applications"."d" = { mode = "0700"; inherit user group; };
     "/home/electro/.local/share/applications/nvim.desktop"."L+".argument =
       let
         drv = pkgs.runCommand "modify-nvim-desktop" {} ''
