b501c7baa85fd91570c9410905d2b3949c0e641b
Gediminas Valys <steamykins@gmail.com>
2024-07-14 22:39:18 +0300

    profiles/neovim: use user tmpfiles instead
    This way, we do not have to recreate the entire path for them with
    additional rules, but the settings interface is not yet merged.

--- profiles/neovim/default.nix
+++ profiles/neovim/default.nix
@@ -1,35 +1,23 @@
 { config, pkgs, ... }:
 
-let
-  user = config.users.users.electro.name;
-  group = config.users.users.electro.group;
-in
-
 {
-  systemd.tmpfiles.settings."10-neovim" = {
-    # Due to how systemd-tmpfiles works, leading directories are implicitly created
-    # if needed, owned by root with an access mode of 0755 by default. In order to
-    # avoid "unsafe path transition" errors, we need to add the appropriate "d" lines.
-    # See tmpfiles.d(5).
-    "/home/electro"."d" = { mode = "0700"; inherit user group; };
-
+  # TODO: Refactor to `systemd.user.tmpfiles.settings` when
+  # https://github.com/NixOS/nixpkgs/pull/317383 is merged.
+  systemd.user.tmpfiles.rules = [
     # Link the Lua config for Neovim.
-    "/home/electro/.config"."d" = { mode = "0755"; inherit user group; };
-    "/home/electro/.config/nvim"."L+".argument = "${./nvim}";
+    "L+ %h/.config/nvim - - - - ${./nvim}"
 
     # Override the Neovim wrapper's .desktop file with our own less weird name.
-    "/home/electro/.local"."d" = { mode = "0755"; inherit user group; };
-    "/home/electro/.local/share"."d" = { mode = "0755"; inherit user group; };
-    "/home/electro/.local/share/applications"."d" = { mode = "0700"; inherit user group; };
-    "/home/electro/.local/share/applications/nvim.desktop"."L+".argument =
+    "L+ %h/.local/share/applications/nvim.desktop - - - - ${
       let
         drv = pkgs.runCommand "modify-nvim-desktop" {} ''
           mkdir -p $out
           substitute ${config.programs.neovim.finalPackage}/share/applications/nvim.desktop $out/nvim.desktop \
             --replace-warn 'Name=Neovim wrapper' 'Name=Neovim'
         '';
-      in "${drv}/nvim.desktop";
-  };
+      in "${drv}/nvim.desktop"
+    }"
+  ];
 
   environment.variables = {
     EDITOR = "nvim";
