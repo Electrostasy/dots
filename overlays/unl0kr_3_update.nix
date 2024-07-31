30f93f117cfaadd82a68ab0c5532031e4cfc21eb
Gediminas Valys <steamykins@gmail.com>
2024-07-31 08:59:38 +0300

    overlays/unl0kr: 3.0.0 -> 3.2.0

--- overlays/unl0kr_3_update.nix
+++ overlays/unl0kr_3_update.nix
@@ -1,13 +1,15 @@
 final: prev: {
   # Contains various fixes since 2.0.0.
   unl0kr = prev.unl0kr.overrideAttrs (finalAttrs: oldAttrs: {
-    version = "3.0.0";
+    version = "3.2.0";
+
     src = oldAttrs.src.override {
       owner = "postmarketOS";
       repo = "buffybox";
       rev = finalAttrs.version;
-      hash = "sha256-xmyh5F6sqD1sOPdocWJtucj4Y8yqbaHfF+a/XOcMk74=";
+      hash = "sha256-nZX7mSY9IBIhVNmOD6mXI1IF2TgyKLc00a8ADAvVLB0=";
     };
+
     sourceRoot = "${finalAttrs.src.name}/unl0kr";
   });
 }
