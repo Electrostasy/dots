2369433af0c5bcb39016fc769d26daa8f27f1cdd
Gediminas Valys <steamykins@gmail.com>
2024-08-25 13:05:47 +0300

    pkgs/nautilus-vimv: use commonpath instead of custom function

--- pkgs/nautilus-vimv/nautilus-vimv.py
+++ pkgs/nautilus-vimv/nautilus-vimv.py
@@ -1,37 +1,21 @@
 from gi.repository import Nautilus, GObject
-from itertools import count
+
 from pathlib import Path
-from subprocess import Popen
-from typing import List
 from urllib.parse import urlparse, unquote
-from os import environ
+import itertools
+import os
+import subprocess
 
 
 class BulkRenameMenuProvider(GObject.GObject, Nautilus.MenuProvider):
-    def _class_name(self) -> str:
-        return self.__class__.__name__
-
     def _uri_to_str(self, file: Nautilus.FileInfo) -> str:
         return unquote(urlparse(file.get_uri()).path)
 
-    def _find_common_cwd(self, files: List[Path]) -> Path:
-        if len(files) > 1:
-            depth = 0
-            for i in count(1):
-                common = set(file.parts[:i] for file in files)
-                if len(common) > 1:
-                    depth = i - 1
-                    break
-            return Path(*files[0].parts[:depth])
-        # When only one entry is selected, the other branch goes into an
-        # infinite loop because of the set, as there is only 1 common root.
-        return Path(*files[0].parts[:-1])
-
-    def _iterate_nautilus_dir(self, dir: Nautilus.FileInfo) -> List[Path]:
+    def _iterate_nautilus_dir(self, dir: Nautilus.FileInfo) -> list[Path]:
         return [entry for entry in Path(self._uri_to_str(dir)).iterdir() if entry.exists()]
 
-    def _open_vimv_for_entries(self, menu, files: List[Path]) -> None:
-        cwd = self._find_common_cwd(files)
+    def _open_vimv_for_entries(self, menu, files: list[Path]) -> None:
+        cwd = os.path.commonpath(files)
         cmd = [
             "xdg-terminal-exec",
             "/usr/bin/env",
@@ -40,9 +24,9 @@ class BulkRenameMenuProvider(GObject.GObject, Nautilus.MenuProvider):
         for file in files:
             cmd.append(file.relative_to(cwd))
 
-        Popen(cmd, cwd=cwd)
+        subprocess.Popen(cmd, cwd=cwd)
 
-    def get_file_items(self, files: List[Nautilus.FileInfo]) -> List[Nautilus.MenuItem]:
+    def get_file_items(self, files: list[Nautilus.FileInfo]) -> list[Nautilus.MenuItem]:
         items = []
         total_entries = len(files)
         if total_entries == 1:
@@ -52,23 +36,23 @@ class BulkRenameMenuProvider(GObject.GObject, Nautilus.MenuProvider):
                 dir_entries = self._iterate_nautilus_dir(files[0])
                 total_entries = len(dir_entries)
                 label = f"Bulk Rename Contents ({total_entries} Items)"
-                item = Nautilus.MenuItem(name=f"{self._class_name()}::Directory", label=label)
+                item = Nautilus.MenuItem(name=f"{self.__class__.__name__}::Directory", label=label)
                 item.connect("activate", self._open_vimv_for_entries, dir_entries)
                 items.append(item)
             label = f"Bulk Rename Selection"
         else:
             label = f"Bulk Rename Selection ({total_entries} Items)"
 
-        item = Nautilus.MenuItem(name=f"{self._class_name()}::Files", label=label)
+        item = Nautilus.MenuItem(name=f"{self.__class__.__name__}::Files", label=label)
         selected_entries = [Path(self._uri_to_str(file)) for file in files if not file.is_gone()]
         item.connect("activate", self._open_vimv_for_entries, selected_entries)
         items.append(item)
         return items
 
-    def get_background_items(self, current_folder: Nautilus.FileInfo) -> List[Nautilus.MenuItem]:
+    def get_background_items(self, current_folder: Nautilus.FileInfo) -> list[Nautilus.MenuItem]:
         dir_entries = self._iterate_nautilus_dir(current_folder)
         total_entries = len(dir_entries)
         label = f"Bulk Rename Contents ({total_entries} Items)"
-        item = Nautilus.MenuItem(name=f"{self._class_name()}::DirectoryBackground", label=label)
+        item = Nautilus.MenuItem(name=f"{self.__class__.__name__}::DirectoryBackground", label=label)
         item.connect("activate", self._open_vimv_for_entries, dir_entries)
         return [item]
