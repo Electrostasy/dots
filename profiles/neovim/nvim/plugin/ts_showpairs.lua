34b2296994b98067a96ac4a58f9b02fb4ad43cca
Gediminas Valys <steamykins@gmail.com>
2025-01-08 13:01:51 +0200

    profiles/neovim: showpairs refactor

--- profiles/neovim/nvim/plugin/ts_showpairs.lua
+++ profiles/neovim/nvim/plugin/ts_showpairs.lua
@@ -6,13 +6,12 @@ local ns = vim.api.nvim_create_namespace('ShowPairs')
 local augroup = vim.api.nvim_create_augroup('ShowPairs', { })
 
 local symbol_pairs = {
-  -- Common pairs.
   { '(', ')' },
   { '{', '}' },
   { '[', ']' },
+  { '<', '>' },
 
-  -- bash command subtitution.
-  -- fish command subtitution.
+  -- Shell command subtitution.
   { '$(', ')' },
 
   -- bash arithmetic expansion.
@@ -23,75 +22,55 @@ local symbol_pairs = {
   { '${', '}' },
 
   -- bash test statement.
-  -- Lua multiline string.
-  -- C++ attribute declaration.
+  -- Lua literal string.
+  -- C++ attribute specifier sequence.
   { '[[', ']]' },
-
-  -- C++ templates.
-  -- Rust type parameters.
-  { '<', '>' },
 }
 
-local lang_to_query_cache = {}
+local _query_cache = {}
 
-local invalidate_tree_queries = function(tree)
-  lang_to_query_cache[tree:lang()] = nil
+_query_cache.on_child_removed = function(tree)
+  _query_cache[tree:lang()] = nil
 end
 
-local cache_tree_queries = function(tree)
+_query_cache.on_child_added = function(tree)
   local lang = tree:lang()
 
-  if lang_to_query_cache[lang] then
+  if _query_cache[lang] then
     return
   end
 
-  local query = {}
-  local symbols = vim.treesitter.language.inspect(lang).symbols
-  for _, pair in pairs(symbol_pairs) do
-    local left_supported = false
-    local right_supported = false
-    for _, value in pairs(symbols) do
-      if not left_supported and value[1] == pair[1] then
-        left_supported = true
+  local symbols = vim.iter(vim.treesitter.language.inspect(lang).symbols)
+    :fold({}, function(acc, _, value)
+      acc[value[1]] = true
+      return acc
+    end)
+
+  _query_cache[lang] = vim.treesitter.query.parse(lang, vim.iter(symbol_pairs)
+    :fold('', function(acc, pair)
+      local opening = pair[1]
+      local closing = pair[2]
+
+      if symbols[opening] and symbols[closing] then
+        acc = acc .. ('(_ (("%s" @opening) ("%s" @closing)))'):format(opening, closing)
       end
 
-      if not right_supported and value[1] == pair[2] then
-        right_supported = true
-      end
-
-      if left_supported and right_supported then
-        table.insert(query, ('(_ (("%s" @opening) ("%s" @closing)))'):format(pair[1], pair[2]))
-        break
-      end
-    end
-  end
-
-  lang_to_query_cache[lang] = vim.treesitter.query.parse(lang, table.concat(query))
+      return acc
+    end))
 end
 
-local remove_highlight = function(bufnr)
+local _remove_highlight = function(bufnr)
   vim.api.nvim_buf_del_extmark(bufnr, ns, 1)
   vim.api.nvim_buf_del_extmark(bufnr, ns, 2)
 end
 
-local add_highlight = function(bufnr)
+local _add_highlight = function(bufnr)
   local cursor = vim.api.nvim_win_get_cursor(0)
   local cursor_range = { cursor[1] - 1, cursor[2], cursor[1] - 1, cursor[2] + 1 }
 
-  -- `LanguageTree:trees()` does not include child languages.
-  local trees = {}
-  vim.treesitter.get_parser():for_each_tree(function(_, tree)
-    if not tree:contains(cursor_range) then
-      return
-    end
-
-    table.insert(trees, tree)
-  end)
-
-  for i = #trees, 1, -1 do
-    local tree = trees[i]
-
-    local query = lang_to_query_cache[tree:lang()]
+  local tree = vim.treesitter.get_parser():language_for_range(cursor_range)
+  while tree do
+    local query = _query_cache[tree:lang()]
     if not query then
       return false
     end
@@ -104,30 +83,21 @@ local add_highlight = function(bufnr)
     end
 
     local node = tree:named_node_for_range(cursor_range)
-
     while node do
       for _, match, _ in query:iter_matches(node, bufnr) do
-        local opening_match, closing_match
-        for id, opening_or_closing_match in pairs(match) do
-          local name = query.captures[id]
+        local opening_start_row, opening_start_col, opening_end_row, opening_end_col = match[1]:range()
+        local closing_start_row, closing_start_col, closing_end_row, closing_end_col = match[2]:range()
 
-          if name == 'opening' then
-            opening_match = opening_or_closing_match
-          elseif name == 'closing' then
-            closing_match = opening_or_closing_match
-          end
-        end
-
-        local opening_start_row, opening_start_col, opening_end_row, opening_end_col = opening_match:range()
-        local closing_start_row, closing_start_col, closing_end_row, closing_end_col = closing_match:range()
-
+        -- Some nodes can contain the matching pair as part of their node, which
+        -- is why we must explicitly check if the cursor is inside the range
+        -- between the pair. Nodes can be mocked with tables in this API, so we
+        -- abuse it to do exactly that.
         local mocked_node = {
           range = function (_, _)
             return opening_start_row, opening_start_col, closing_end_row, closing_end_col
           end
         }
 
-        -- Nodes can be mocked with tables in this API, so let's abuse it.
         ---@diagnostic disable: missing-fields
         if vim.treesitter.node_contains(mocked_node, cursor_range) then
           vim.api.nvim_buf_set_extmark(bufnr, ns, opening_start_row, opening_start_col, {
@@ -152,6 +122,9 @@ local add_highlight = function(bufnr)
 
       node = node:parent()
     end
+
+    ---@diagnostic disable-next-line: invisible
+    tree = tree:parent()
   end
 
   return false
@@ -166,24 +139,22 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'FileType' }, {
       return
     end
 
-    -- Register callbacks for each added language tree to cache the queries,
-    -- doing it on every CursorMoved event is feasible but some parsers like
-    -- markdown seem to struggle.
     parser:register_cbs({
-      on_child_removed = invalidate_tree_queries,
-      on_child_added = cache_tree_queries,
+      on_child_removed = _query_cache.on_child_removed,
+      on_child_added = _query_cache.on_child_added,
     })
 
-    -- Perform an initial pass to cache the queries.
+    -- The callbacks are not called until a tree is added/removed, so we need
+    -- to perform a first pass.
     parser:for_each_tree(function(_, tree)
-      cache_tree_queries(tree)
+      _query_cache.on_child_added(tree)
     end)
 
     vim.api.nvim_create_autocmd('BufLeave', {
       group = augroup,
       buffer = event.buf,
       callback = function()
-        remove_highlight(event.buf)
+        _remove_highlight(event.buf)
       end,
     })
 
@@ -191,8 +162,8 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'FileType' }, {
       group = augroup,
       buffer = event.buf,
       callback = function()
-        if not add_highlight(event.buf) then
-          remove_highlight(event.buf)
+        if not _add_highlight(event.buf) then
+          _remove_highlight(event.buf)
         end
       end,
     })
