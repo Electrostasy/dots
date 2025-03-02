8b314622f64c41e97f547b9d52a94345950e8d33
Gediminas Valys <steamykins@gmail.com>
2025-03-02 19:54:35 +0200

    profiles/neovim: enable ibl for tab indentation

--- profiles/neovim/nvim/after/plugin/indent-blankline.lua
+++ profiles/neovim/nvim/after/plugin/indent-blankline.lua
@@ -1,6 +1,9 @@
 -- :h ibl.config
 require('ibl').setup({
-  indent = { char = '▏' },
+  indent = {
+    char = '▏',
+    tab_char = '▏',
+  },
   scope = {
     show_start = false,
     show_end = false
