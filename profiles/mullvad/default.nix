a81a6b57c3b097432d359ac87ea7a7a320377d62
Gediminas Valys <steamykins@gmail.com>
2025-04-26 11:03:29 +0300

    profiles/mullvad: accept incoming tailscale connections

--- profiles/mullvad/default.nix
+++ profiles/mullvad/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   sops.secrets.mullvadAccount = {
@@ -48,26 +48,61 @@
     '';
   };
 
-  networking.nftables = lib.mkIf config.services.tailscale.enable {
-    enable = true;
+  # Mullvad does not let any incoming LAN connections on subnets other than the
+  # ones they have whitelisted (for IPv4 at the time of writing: 10.0.0.0/8,
+  # 172.16.0.0/12, 192.168.0.0/16, 169.254.0.0/16), so, in practice, incoming
+  # Tailscale connections cannot be accepted with just firewall rules:
+  # https://github.com/mullvad/mullvadvpn-app/issues/6086
+  # https://github.com/mullvad/mullvadvpn-app/blob/045a5c33f140945072e42553939adcf7bace52c1/talpid-types/src/net/mod.rs#L24
+  # As a workaround, for outgoing connections, the outgoing nftables chain is
+  # enough. However, for incoming connections, we require BOTH the prerouting
+  # nftables chain as well as the NetworkManager dispatcher script to add the
+  # route. Without these, a host running Mullvad, even with LAN sharing
+  # enabled, will completely drop all ICMP and other incoming requests over
+  # Tailscale.
+  networking = lib.mkIf config.services.tailscale.enable {
+    nftables = {
+      enable = true;
 
-    # Mullvad and Tailscale will fight to the death over routing rules (and
-    # Mullvad will win) unless we set exceptions for Tailscale:
-    # https://github.com/tailscale/tailscale/issues/925#issuecomment-1616354736.
-    tables."mullvad-tailscale" = {
-      family = "inet";
-      content = ''
-        chain prerouting {
-          type filter hook prerouting priority -100; policy accept;
-          ip saddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-        }
+      tables."ts-mullvad" = {
+        family = "inet";
 
-        chain outgoing {
-          type route hook output priority -100; policy accept;
-          meta mark 0x80000 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-          ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        # Marks traffic with a connection tracking mark (0x00000f41) to get
+        # through the firewall and a meta mark (0x6d6f6c65) to route the traffic
+        # outside the tunnel:
+        # https://mullvad.net/en/help/split-tunneling-with-linux-advanced#allow-incoming
+        content = ''
+          chain prerouting {
+            type filter hook prerouting priority -100; policy accept;
+            ip saddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+
+          chain outgoing {
+            type route hook output priority -100; policy accept;
+            ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+        '';
+      };
+    };
+
+    networkmanager = {
+      enable = true;
+
+      dispatcherScripts = [
+        {
+          # https://github.com/mullvad/mullvadvpn-app/issues/6833#issuecomment-2387277203
+          source = pkgs.writeShellScript "add-tailscale-route.sh" ''
+            if [ "$1" == "tailscale0" ]; then
+              if [ "$2" == "up" ]; then
+                ip route add 100.64.0.0/10 dev tailscale0 table main
+              elif [ "$2" == "down" ]; then
+                ip route del 100.64.0.0/10 dev tailscale0 table main
+              fi
+            fi
+          '';
+          type = "basic";
         }
-      '';
+      ];
     };
   };
 }
