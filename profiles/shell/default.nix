9f3be71cb192b6b2ce055fbbaa1bbc708f98a764
Gediminas Valys <steamykins@gmail.com>
2025-06-21 10:57:51 +0300

    profiles/shell: add ? function to print nix shell packages

--- profiles/shell/default.nix
+++ profiles/shell/default.nix
@@ -60,12 +60,34 @@
     enable = true;
 
     interactiveShellInit = /* fish */ ''
-      set -g fish_greeting # Disable greeting.
+      set -g fish_greeting # disable greeting.
 
       function ls --wraps eza
         set -l flags (path filter -v -- $argv | string match -rg '^\./(-.*)$')
         set -l entries (path normalize -- $argv | string match -rv '^\./' | path resolve; or pwd)
-        command eza -TL1 --group-directories-first --icons=auto $flags $entries
+        command eza -TL1 --binary --group-directories-first --icons=auto $flags $entries
+      end
+
+      function ? --description 'Print a list of all executables provided by this Nix shell'
+        if test $SHLVL -gt 1 && string match -q -- '/nix/store/*' $PATH[1]
+          echo 'The following executables are provided by this ephemeral shell:'
+          set -f input
+          set -f id 1
+          for store_path in (string match -- '/nix/store/*' $PATH | path filter | sort -k 1.44)
+            set -f name (nix derivation show $store_path | jq -r '.[].env | .version as $version | .name | sub("-\($version).*"; "")')
+            set -f parent $id
+            set -a input "$id $parent $name"
+            set id (math "$id + 1")
+            for executable in (fd . $store_path -t x -L --format '{/}' | sort -u)
+              set -a input "$id $parent $executable"
+              set id (math "$id + 1")
+            end
+          end
+
+          printf '%s\n' $input | column --tree-id 1 --tree-parent 2 --tree 3 --table-hide 1,2
+        else
+          echo 'Not in Nix shell!'
+        end
       end
 
       set -Ux fish_color_cancel red --reverse
