b5c5dbbc51ecced37b6dec0b84fd3df1c8fc5092
Gediminas Valys <steamykins@gmail.com>
2025-06-21 20:16:35 +0300

    profiles/shell: use 2 spaces indented output in fish_command_not_found

--- profiles/shell/functions/fish_command_not_found.fish
+++ profiles/shell/functions/fish_command_not_found.fish
@@ -12,12 +12,12 @@ function fish_command_not_found
 		end
 
 		set -a output "\nYou can make it available in an ephemeral shell by typing$plural_text the following:\n"
-		set -a output \t(printf 'nix shell nixpkgs#%s\n' $packages | fish_indent --ansi)\n
+		set -a output ' '(printf 'nix shell nixpkgs#%s\n' $packages | fish_indent --ansi)\n
 		set -e output[-1] # remove the final newline.
 
 		# FIXME: We shouldn't need set_color here, some escape probably got cut.
 		set -a output (set_color $fish_color_normal; echo -n "\nYou can run it once by typing$plural_text the following:\n")
-		set -a output \t(printf 'nix run nixpkgs#%s\n' $packages | fish_indent --ansi)\n
+		set -a output ' '(printf 'nix run nixpkgs#%s\n' $packages | fish_indent --ansi)\n
 		set -e output[-1] # remove the final newline.
 	else
 		set -a output 'It is not provided by any indexed packages.\n'
