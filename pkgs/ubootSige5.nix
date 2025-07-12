8eb6560f32d0d8f99e127317672a85b07bcc15d4
Gediminas Valys <steamykins@gmail.com>
2025-07-12 22:12:08 +0300

    pkgs/ubootSige5: switch to mainline u-boot

--- pkgs/ubootSige5.nix
+++ pkgs/ubootSige5.nix
@@ -1,22 +1,53 @@
 {
   buildUBoot,
-  fetchFromGitLab,
+  fetchurl,
   armTrustedFirmwareRK3576,
   rkbin,
 }:
 
 buildUBoot {
-  version = "0-unstable-2025-07-08";
-
-  src = fetchFromGitLab {
-    domain = "gitlab.collabora.com";
-    owner = "hardware-enablement/rockchip-3588";
-    repo = "u-boot";
-    rev = "eaadfde0c6a8975c85d9da9e19a5af70795cd1c6";
-    hash = "sha256-/7kFYtKkZwbhMVxfRXTQZgRWrOpswkvUHHYmcRhk2Mc=";
-  };
-
-  patches = [ ];
+  patches = [
+    # Mainline U-Boot cannot be booted on RK3576 boards without these patches, see:
+    # https://patchwork.ozlabs.org/project/uboot/patch/20250627025721.962397-1-liujianfeng1994@gmail.com/
+    # https://patchwork.ozlabs.org/project/uboot/cover/20260311-rk3576-ufs-v6-0-c7c353739242@flipper.net/
+    # [0/7] rockchip: fix boot from SDMMC on RK3576
+    # https://patchwork.ozlabs.org/cover/2269849
+    (fetchurl {
+      name = "0001-1-7-rockchip-mkimage-Split-size_and_off-and-size_and_nimage.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269850/raw";
+      hash = "sha256-lVmXjW8cBdFGsd81nOyzvK/MsmIqi4VmH7bngY5Jahs=";
+    })
+    (fetchurl {
+      name = "0002-2-7-rockchip-mkimage-Print-image-information-for-all-embedded-images.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269851/raw";
+      hash = "sha256-ZVCurOfalQ+AI41J0VwVHiWq1mxFL8itum/FBxoR+nI=";
+    })
+    (fetchurl {
+      name = "0003-3-7-rockchip-mkimage-Print-boot0-and-boot1-parameters.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269853/raw";
+      hash = "sha256-sn4T/1ST/8Tjg65CwgoQuFpjOdDG71zlWwcmABrVKFo=";
+    })
+    (fetchurl {
+      name = "0004-4-7-rockchip-mkimage-Add-option-to-change-image-offset-alignment.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269854/raw";
+      hash = "sha256-NvKoof5qPE4SAJcjHuf6ObLDj+i+l/tjoJ15VowF90A=";
+    })
+    (fetchurl {
+      name = "0005-5-7-rockchip-mkimage-Add-support-for-up-to-4-input-files.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269855/raw";
+      hash = "sha256-8kmGhs89KR/M4rKaTBrWdsHZeQBigqjH7seANOEq1RE=";
+    })
+    (fetchurl {
+      name = "0006-6-7-rockchip-mkimage-Add-option-for-image-load-address-and-flag.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269856/raw";
+      hash = "sha256-WUw01N1pLyh9uMSnb0M3RCR7O9RZLVczuQiYsn5G3N8=";
+    })
+    (fetchurl {
+      name = "0007-7-7-rockchip-mkimage-Add-rk3576-align-and-sd-card-workaround.diff";
+      url = "https://patchwork.ozlabs.org/patch/2269857/raw";
+      hash = "sha256-7cxU4iyZryzzPaDA4fVBAjLZWokFgNyYHj8QpkJy0zg=";
+    })
+  ];
 
   defconfig = "sige5-rk3576_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
