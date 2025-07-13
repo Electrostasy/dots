f2246cbb758321ec98a62e031bf796a575a98931
Gediminas Valys <steamykins@gmail.com>
2025-07-13 13:13:54 +0300

    pkgs/ubootSige5: 0-unstable-2025-04-29 -> 0-unstable-2025-07-08

--- pkgs/ubootSige5.nix
+++ pkgs/ubootSige5.nix
@@ -1,24 +1,19 @@
-{ buildUBoot
-, fetchFromGitLab
-, armTrustedFirmwareRK3576
-, rkbin
+{
+  buildUBoot,
+  fetchFromGitLab,
+  armTrustedFirmwareRK3576,
+  rkbin,
 }:
 
 buildUBoot {
-  version = "0-unstable-2025-04-29";
+  version = "0-unstable-2025-07-08";
 
-  # TODO: Switch to upstream whenever support lands.
-  # U-Boot does not yet have a stable release with RK3576 SoC support, but
-  # Collabora have a lot of convenient patches on top of upstream's
-  # v2025.07-rc1 (including patches sent and not yet sent to the mailing list,
-  # like ArmSoM Sige5 board support:
-  # https://patchwork.ozlabs.org/project/uboot/list/?series=453774).
   src = fetchFromGitLab {
     domain = "gitlab.collabora.com";
     owner = "hardware-enablement/rockchip-3588";
     repo = "u-boot";
-    rev = "667bbdc907027bddacb8916428677c1db2e084e6";
-    hash = "sha256-05m0vrXlHXohVx25Js7qrhHEl4qzhivwRyw9aUY33Ds=";
+    rev = "eaadfde0c6a8975c85d9da9e19a5af70795cd1c6";
+    hash = "sha256-/7kFYtKkZwbhMVxfRXTQZgRWrOpswkvUHHYmcRhk2Mc=";
   };
 
   patches = [ ];
@@ -26,6 +21,6 @@ buildUBoot {
   defconfig = "sige5-rk3576_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
   BL31 = "${armTrustedFirmwareRK3576}/bl31.elf";
-  ROCKCHIP_TPL = "${rkbin}/bin/rk35/rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.08.bin";
+  ROCKCHIP_TPL = "${rkbin}/bin/rk35/rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.09.bin";
   filesToInstall = [ "u-boot.itb" "idbloader.img" "u-boot-rockchip.bin" ];
 }
