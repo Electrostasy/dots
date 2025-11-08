37e4da5a4a978cb348e632e66812ec8592ce61a0
Gediminas Valys <steamykins@gmail.com>
2025-11-08 15:13:15 +0200

    hosts/terra: update audio settings
    - Persist wireplumber state instead of using a wpexec script
    - Refactor parametric EQ and add another EQ preset
    - Setup global 7.1 surround sound spatializer using ClubFritz4.sofa
    - Add mpv 7.1 surround sound profile to use the correct audio device

--- hosts/terra/audio.nix
+++ hosts/terra/audio.nix
@@ -1,14 +1,24 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
+
+let
+  sofa = pkgs.fetchurl {
+    url = "https://sofacoustics.org/data/database/clubfritz/ClubFritz4.sofa";
+    hash = "sha256-V6rFKb+MycLFHemw6bUGLB1odugqKiMMErAizudZsHY=";
+  };
+in
 
 {
-  # Use WirePlumber `wpexec` script to set default nodes, as it overrides
-  # `default.configured.audio.{sink,source}` from PipeWire's `context.properties`.
-  systemd.user.services.wireplumber-defaults = {
-    description = "WirePlumber set default nodes";
-    after = [ "wireplumber.service" ];
-    wantedBy = [ "wireplumber.service" ];
+  preservation.preserveAt."/persist/state".users.electro.directories = [
+    ".local/state/wireplumber"
+  ];
 
-    serviceConfig.ExecStart = "${config.services.pipewire.wireplumber.package}/bin/wpexec ${./wpexec-defaults.lua}";
+  # mpv cannot play multichannel audio with the "default" device due to ALSA:
+  # https://github.com/mpv-player/mpv/commit/af3bbb800d709f81c9f30bc4ced26c98ea5eafd6
+  programs.mpv.settings.surround_7_1 = {
+    profile-desc = "Choose 7.1 Surround audio device";
+    profile-cond = "audio_params[\"channel-count\"] > 2";
+    profile-restore = "copy";
+    audio-device = "pipewire/input.7_1_spatializer";
   };
 
   security.rtkit.enable = true;
@@ -17,179 +27,238 @@
     enable = true;
     pulse.enable = true;
 
-    wireplumber.extraConfig = {
-      # Volume is by default set to 0.4.
-      "60-defaults"."wireplumber.settings"."device.routes.default-sink-volume" = 1.0;
+    extraConfig.pipewire = {
+      "60-parametric-equalizer" = {
+        "context.modules" = [
+          { name = "libpipewire-module-rtkit"; args = { }; flags = [ "ifexists" "nofail" ]; }
 
-      "60-disable-devices"."monitor.alsa.rules" = [
-        {
-          matches = [ { "device.product.name" = "Navi 21/23 HDMI/DP Audio Controller"; } ];
-          actions.update-props."device.disabled" = true;
-        }
-      ];
+          # https://github.com/jaakkopasanen/AutoEq/tree/master/results/oratory1990.
+          {
+            name = "libpipewire-module-parametric-equalizer";
+            args = {
+              "node.description" = "HIFIMAN Sundara";
+              "media.name" = "HIFIMAN Sundara";
+              "equalizer.filepath" = ./HIFIMAN-Sundara-Dekoni-sheepskin-Earpads-ParametricEQ.txt;
+              "equalizer.description" = "Parametric Equalizer sink for HIFIMAN Sundara headphones";
+              "audio.channels" = 2;
+              "audio.position" = [ "FL" "FR" ];
 
-      # Disable S/PDIF.
-      "60-disable-spdif"."monitor.alsa.rules" = [
-        {
-          matches = [
-            { "device.nick" = "Fifine K658  Microphone"; }
-            { "device.nick" = "JDS Labs EL DAC II+"; }
-          ];
-          actions."update-props"."device.profile-set" = "analog-only.conf";
-        }
-      ];
-    };
-
-    # EQ for HIFIMAN Sundara headphones based on 10 band parametric EQ from:
-    # https://github.com/jaakkopasanen/AutoEq/tree/master/results/oratory1990
-    extraConfig.pipewire."60-hifiman-sundara" = {
-      "context.modules" = [
-        { name = "libpipewire-module-rtkit"; args = { }; flags = [ "ifexists" "nofail" ]; }
-        { name = "libpipewire-module-filter-chain";
-          args = {
-            "node.description" = "HIFIMAN Sundara";
-            "media.name" = "HIFIMAN Sundara";
-            "filter.graph" =
-              let
-                # Gain is calculated as pow(10, db/20).
-                nodes = [
-                  {
-                    type = "builtin";
-                    name = "mixerI";
-                    label = "mixer";
-                    control = { "Gain 1" = 0.457088; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_1";
-                    label = "bq_lowshelf";
-                    control = { Freq = 105.0; Q = 0.7; Gain = 1.496236; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_2";
-                    label = "bq_peaking";
-                    control = { Freq = 197.0; Q = 0.18; Gain = 0.860994; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_3";
-                    label = "bq_peaking";
-                    control = { Freq = 1950.0; Q = 1.84; Gain = 2.113489; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_4";
-                    label = "bq_peaking";
-                    control = { Freq = 3797.0; Q = 1.05; Gain = 0.595662; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_5";
-                    label = "bq_peaking";
-                    control = { Freq = 6109.0; Q = 3.33; Gain = 1.798871; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_6";
-                    label = "bq_peaking";
-                    control = { Freq = 71.0; Q = 1.84; Gain = 0.944061; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_7";
-                    label = "bq_peaking";
-                    control = { Freq = 458.0; Q = 2.9; Gain = 1.148154; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_8";
-                    label = "bq_peaking";
-                    control = { Freq = 824.0; Q = 2.49; Gain = 0.891251; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_9";
-                    label = "bq_peaking";
-                    control = { Freq = 9193.0; Q = 2.74; Gain = 1.513561; };
-                  }
-                  {
-                    type = "builtin";
-                    name = "eq_10";
-                    label = "bq_highshelf";
-                    control = { Freq = 10000.0; Q = 0.7; Gain = 0.616595; };
-                  }
-                ];
-              in
-              {
-                inherit nodes;
-
-                inputs = [ "mixerI:In 1" ];
-                outputs = [ "eq_10:Out" ];
-
-                links =
-                  lib.zipListsWith
-                    (outNode: inNode: {
-                      input = "${inNode.name}:In";
-                      output = "${outNode.name}:Out";
-                    })
-                    (lib.init nodes)
-                    (lib.tail nodes);
+              "capture.props" = {
+                "node.name" = "input.hifiman_sundara_eq";
               };
 
-            "audio.channels" = 2;
-            "audio.position" = [ "FL" "FR" ];
-
-            "capture.props" = {
-              "node.name" = "hifiman_sundara_input";
-              "media.class" = "Audio/Sink";
+              "playback.props" = {
+                "node.name" = "output.hifiman_sundara_eq";
+              };
             };
+          }
+          {
+            name = "libpipewire-module-parametric-equalizer";
+            args = {
+              "node.description" = "HIFIMAN Sundara Closed-back";
+              "media.name" = "HIFIMAN Sundara Closed-back";
+              "equalizer.filepath" = ./HIFIMAN-Sundara-Closed-Back-ParametricEQ.txt;
+              "equalizer.description" = "Parametric Equalizer sink for HIFIMAN Sundara Closed-back headphones";
+              "audio.channels" = 2;
+              "audio.position" = [ "FL" "FR" ];
 
-            "playback.props" = {
-              "node.name" = "hifiman_sundara_output";
-              "node.passive" = true;
+              "capture.props" = {
+                "node.name" = "input.hifiman_sundara_closed_back_eq";
+              };
+
+              "playback.props" = {
+                "node.name" = "output.hifiman_sundara_closed_back_eq";
+              };
             };
-          };
-        }
-      ];
+          }
+        ];
+      };
+
+      "60-spatializer" = {
+        "context.modules" = [
+          { name = "libpipewire-module-rtkit"; args = { }; flags = [ "ifexists" "nofail" ]; }
+
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "7.1 Surround Sound Spatializer";
+              "media.name" = "7.1 Surround Sound Spatializer";
+              "filter.graph" = {
+                nodes = [
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "FL";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 30.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "FR";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 330.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "FC";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 0.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "RL";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 150.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "RR";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 210.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "SL";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 90.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "SR";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 270.0; "Elevation" = 0.0; "Radius" = 3.0; };
+                  }
+                  {
+                    type = "sofa";
+                    label = "spatializer";
+                    name = "LFE";
+                    config.filename = "${sofa}";
+                    control = { "Azimuth" = 0.0; "Elevation" = -60.0; "Radius" = 3.0; };
+                  }
+                  { type = "builtin"; label = "mixer"; name = "mixerL"; }
+                  { type = "builtin"; label = "mixer"; name = "mixerR"; }
+                ];
+
+                links = [
+                  { output = "FL:Out L"; input = "mixerL:In 1"; }
+                  { output = "FL:Out R"; input = "mixerR:In 1"; }
+                  { output = "FR:Out L"; input = "mixerL:In 2"; }
+                  { output = "FR:Out R"; input = "mixerR:In 2"; }
+                  { output = "FC:Out L"; input = "mixerL:In 3"; }
+                  { output = "FC:Out R"; input = "mixerR:In 3"; }
+                  { output = "RL:Out L"; input = "mixerL:In 4"; }
+                  { output = "RL:Out R"; input = "mixerR:In 4"; }
+                  { output = "RR:Out L"; input = "mixerL:In 5"; }
+                  { output = "RR:Out R"; input = "mixerR:In 5"; }
+                  { output = "SL:Out R"; input = "mixerR:In 6"; }
+                  { output = "SL:Out L"; input = "mixerL:In 6"; }
+                  { output = "SR:Out R"; input = "mixerR:In 7"; }
+                  { output = "SR:Out L"; input = "mixerL:In 7"; }
+                  { output = "LFE:Out R"; input = "mixerR:In 8"; }
+                  { output = "LFE:Out L"; input = "mixerL:In 8"; }
+                ];
+
+                inputs = [ "FL:In" "FR:In" "FC:In" "RL:In" "RR:In" "SL:In" "SR:In" "LFE:In" ];
+                outputs = [ "mixerL:Out" "mixerR:Out" ];
+              };
+
+              "capture.props" = {
+                "node.name" = "input.7_1_spatializer";
+                "media.class" = "Audio/Sink";
+                "audio.channels" = 8;
+                "audio.position" = [ "FL" "FR" "FC" "LFE" "RL" "RR" "SL" "SR" ];
+              };
+
+              "playback.props" = {
+                "node.name" = "output.7_1_spatializer";
+                "node.passive" = true;
+                "audio.channels" = 2;
+                "audio.position" = [ "FL" "FR" ];
+              };
+            };
+          }
+        ];
+      };
+
+      "60-microphone-denoiser" = {
+        "context.modules" = [
+          { name = "libpipewire-module-rtkit"; args = { }; flags = [ "ifexists" "nofail" ]; }
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "Microphone (noise suppressed)";
+              "media.name" = "Microphone (noise suppressed)";
+              "filter.graph" = {
+                nodes = [
+                  {
+                    type = "ladspa";
+                    name = "rnnoise";
+                    plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    label = "noise_suppressor_mono";
+                    control = {
+                      "VAD Threshold (%)" = 85.0;
+                      "VAD Grace Period (ms)" = 500;
+                      "Retroactive VAD Grace (ms)" = 0;
+                    };
+                  }
+                ];
+              };
+              "audio.rate" = 48000;
+              "audio.position" = [ "FL" ];
+
+              "capture.props" = {
+                "node.passive" = true;
+                "node.name" = "input.microphone_rnnoise";
+              };
+
+              "playback.props" = {
+                "media.class" = "Audio/Source";
+                "node.name" = "output.microphone_rnnoise";
+              };
+            };
+          }
+        ];
+      };
     };
 
-    # RNNoise Noise Cancelling Source for microphone:
-    # https://github.com/werman/noise-suppression-for-voice
-    extraConfig.pipewire."60-microphone-rnnoise" = {
-      "context.modules" = [
-        { name = "libpipewire-module-filter-chain";
-          args = {
-            "node.description" = "Microphone (noise suppressed)";
-            "media.name" = "Microphone (noise suppressed)";
-            "filter.graph" = {
-              nodes = [
-                {
-                  type = "ladspa";
-                  name = "rnnoise";
-                  plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-                  label = "noise_suppressor_mono";
-                  control = {
-                    "VAD Threshold (%)" = 85.0;
-                    "VAD Grace Period (ms)" = 500;
-                    "Retroactive VAD Grace (ms)" = 0;
-                  };
-                }
-              ];
+    wireplumber.extraConfig = {
+      "60-defaults" = {
+        "wireplumber.settings" = {
+          "device.routes.default-sink-volume" = 1.0;
+        };
+      };
+
+      "60-disabled-devices" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              { "device.product.name" = "Navi 21/23 HDMI/DP Audio Controller"; }
+            ];
+            actions = {
+              update-props = {
+                "device.disabled" = true;
+              };
             };
-            "audio.rate" = 48000;
-            "audio.position" = [ "FL" ];
-            "capture.props" = {
-              "node.passive" = true;
-              "node.name" = "rnnoise_input";
+          }
+          {
+            matches = [
+              { "device.nick" = "Fifine K658  Microphone"; }
+              { "device.nick" = "JDS Labs EL DAC II+"; }
+            ];
+            actions = {
+              update-props = {
+                "device.profile-set" = "analog-only.conf";
+              };
             };
-            "playback.props" = {
-              "media.class" = "Audio/Source";
-              "node.name" = "rnnoise_output";
-            };
-          };
-        }
-      ];
+          }
+        ];
+      };
     };
   };
 }
