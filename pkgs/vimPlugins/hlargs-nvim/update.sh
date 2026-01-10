29b20bed92937a005d1f2f8ec1c8f256f6f6b9e3
Gediminas Valys <steamykins@gmail.com>
2026-01-10 18:49:44 +0200

    pkgs/vimPlugins: remove
    The only remaining plugin here, hlargs-nvim, apparently got packaged in
    nixpkgs half a year ago.

--- pkgs/vimPlugins/hlargs-nvim/update.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env -S nix env shell nixpkgs#nix-update --command bash
-
-(cd "$(git rev-parse --show-toplevel)" && nix-update --flake --version=branch "legacyPackages.$(nix eval --raw --impure --expr 'builtins.currentSystem').vimPlugins.hlargs-nvim")
