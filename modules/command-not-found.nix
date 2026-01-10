84641490a2657b5d8530b60a773adff251acf332
Gediminas Valys <steamykins@gmail.com>
2026-01-10 18:31:36 +0200

    {modules,pkgs}/command-not-found: use shell-specific scripts
    It is easier to integrate shell specific features (such as syntax
    highlighting) when we are not using one script to handle the command not
    found case. Changes include:
    
    - pkgs/command-not-found.nix is removed and reintegrated into
      modules/command-not-found.nix as a bash function instead of a script.
    - A fish shell specific reimplementation of pkgs/command-not-found.nix
      is added.
    - No more zsh support, though the same bash function works in practice.
    - Because we are no longer calling fish_indent from a bash script called
      from fish shell, we now have proper syntax highlighting using the
      current fish shell theme - fish_indent uses the colours defined in the
      environment, which is not available in bash.

--- modules/command-not-found.nix
+++ modules/command-not-found.nix
@@ -5,8 +5,6 @@ let
 in
 
 {
-  # TODO: If https://github.com/NixOS/nixpkgs/pull/415070 is merged, remove
-  # these and set our command-not-found as programs.command-not-found.package.
   disabledModules = [ "programs/command-not-found/command-not-found.nix" ];
 
   options.programs.command-not-found = {
@@ -17,15 +15,6 @@ in
       Requires either nix-channels to be set and downloaded (sudo nix-channels
       --update) or the nixpkgs flake input set to a lockable tarball
       (https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz)'' // { default = true; };
-
-    package = lib.mkPackageOption pkgs "command-not-found" { };
-
-    finalPackage = lib.mkOption {
-      type = lib.types.package;
-      visible = false;
-      readOnly = true;
-      description = "Resulting customized command-not-found package.";
-    };
 
     dbPath = lib.mkOption {
       default = "${pkgs.path}/programs.sqlite";
@@ -39,29 +28,80 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.command-not-found.finalPackage = cfg.package.override {
-      inherit (cfg) dbPath;
-    };
-
-    programs.bash.interactiveShellInit = ''
+    programs.bash.interactiveShellInit = /* bash */ ''
       command_not_found_handle() {
-        "${lib.getExe cfg.finalPackage}" "$@"
+        if [[ ! -e '${cfg.dbPath}' ]]; then
+          echo '${cfg.dbPath} is missing!'
+          echo 'Please set programs.command-not-found.dbPath in your NixOS configuration to where programs.sqlite is.'
+          echo 'You can disable this feature by setting programs.command-not-found.enable to false in your NixOS configuration.'
+          return 127
+        fi
+
+        mapfile -t packages < <(${lib.getExe pkgs.sqlite} '${cfg.dbPath}' "SELECT DISTINCT package FROM Programs WHERE name = '$1';")
+        amount=''${#packages[@]}
+
+        output="The program '$1' is not in your PATH. "
+
+        if [[ $amount -eq 0 ]]; then
+          output+='It is not provided by any indexed packages.'
+        else
+          maybe_one_of=
+          if [[ $amount -eq 1 ]]; then
+            output+='It is provided by one package.\n'
+          else
+            output+='It is provided by several packages.\n'
+            maybe_one_of=' one of'
+          fi
+
+          output+="You can make it available in an ephemeral shell by typing$maybe_one_of the following:\n"
+          output+="$(printf '  nix shell nixpkgs#%s\n' "''${packages[@]}")\n\n"
+          output+="You can run it once by typing$maybe_one_of the following:\n"
+          output+="$(printf '  nix run nixpkgs#%s\n' "''${packages[@]}")"
+        fi
+
+        printf '%b' "$output"
       }
     '';
 
-    programs.zsh.interactiveShellInit = ''
-      command_not_found_handler() {
-        "${lib.getExe cfg.finalPackage}" "$@"
-      }
-    '';
+    # INFO: I don't use zsh.
+    # programs.zsh.interactiveShellInit = ''
+    #   command_not_found_handler() {
+    #   }
+    # '';
 
-    # NOTE: Fish itself checks for command-not-found, set it explicitly.
-    programs.fish.interactiveShellInit = ''
+    programs.fish.interactiveShellInit = /* fish */ ''
       function fish_command_not_found
-        "${lib.getExe cfg.finalPackage}" $argv
+        if not test -e ${cfg.dbPath}
+          echo '${cfg.dbPath} is missing!'
+          echo 'Please set programs.command-not-found.dbPath in your NixOS configuration to where programs.sqlite is.'
+          echo 'You can disable this feature by setting programs.command-not-found.enable to false in your NixOS configuration.'
+          return 127
+        end
+
+        set -l packages (${lib.getExe pkgs.sqlite} '${cfg.dbPath}' "SELECT DISTINCT package FROM Programs WHERE name = '$argv[1]';")
+        set -l amount (count $packages)
+
+        set -l output "The program '$(set_color $fish_color_error; echo -n $argv[1]; set_color normal)' is not in your PATH. "
+
+        if test $amount -eq 0
+          set -a output 'It is not provided by any indexed packages.\n'
+        else
+          if test $amount -eq 1
+            set -a output 'It is provided by one package.\n'
+          else
+            set -a output 'It is provided by several packages.\n'
+            set -l maybe_one_of ' one of'
+          end
+
+          set -a output \
+            "You can make it available in an ephemeral shell by typing$maybe_one_of the following:\n" \
+            "$(printf '  nix shell nixpkgs#%s\n' $packages | fish_indent --ansi --only-unindent)" '\n\n' \
+            "You can run it once by typing$maybe_one_of the following:\n" \
+            "$(printf '  nix run nixpkgs#%s\n' $packages | fish_indent --ansi --only-unindent)" '\n'
+        end
+
+        printf '%b' $output
       end
     '';
-
-    environment.systemPackages = [ cfg.finalPackage ];
   };
 }
