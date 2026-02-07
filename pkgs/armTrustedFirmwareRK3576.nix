abc1358b291d9814c7a55d31976a3ffcb9de83b0
Gediminas Valys <steamykins@gmail.com>
2026-02-07 14:39:19 +0200

    pkgs/armTrustedFirmwareRK3576: switch to upstream source

--- pkgs/armTrustedFirmwareRK3576.nix
+++ pkgs/armTrustedFirmwareRK3576.nix
@@ -1,20 +1,12 @@
-{ buildArmTrustedFirmware
-, fetchFromGitHub
+{
+  buildArmTrustedFirmware,
 }:
 
 buildArmTrustedFirmware rec {
-  # 2.12.1 currently packaged upstream does not have RK3576 support.
-  version = "2.13.0";
-
-  src = fetchFromGitHub {
-    owner = "ARM-software";
-    repo = "arm-trusted-firmware";
-    tag = "v${version}";
-    hash = "sha256-rxm5RCjT/MyMCTxiEC8jQeFMrCggrb2DRbs/qDPXb20=";
-  };
-
   extraMakeFlags = [ "bl31" ];
   platform = "rk3576";
   extraMeta.platforms = [ "aarch64-linux" ];
   filesToInstall = [ "build/${platform}/release/bl31/bl31.elf" ];
+
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=unterminated-string-initialization";
 }
