344e5f35132f443e534ccbc3e88428514624bb2e
Gediminas Valys <steamykins@gmail.com>
2026-02-28 09:17:30 +0200

    hosts/phobos: remove discord transcriber bot

--- hosts/phobos/default.nix
+++ hosts/phobos/default.nix
@@ -8,7 +8,6 @@
     ../../profiles/tailscale.nix
     ../../profiles/zramswap.nix
     ../../users/electro
-    ./discord-transcriber.nix
     ./fileserver.nix
     ./headscale.nix
     ./hostapd.nix
