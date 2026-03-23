703aaa57a45647a40d8cae99da8b4fee274ebace
Gediminas Valys <steamykins@gmail.com>
2026-03-23 11:35:41 +0200

    hosts: refactor host generation to be simpler
    Flake inputs are not added often, we do not need to fetch their
    nixosModules.default flake output, with this we can use lib.genAttrs.

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,43 +3,27 @@
 let
   inherit (nixpkgs) lib;
 
-  inputModules = lib.pipe self.inputs [
-    (lib.filterAttrs (_: lib.hasAttrByPath [ "nixosModules" "default" ]))
-    (lib.mapAttrsToList (_: lib.getAttrFromPath [ "nixosModules" "default" ]))
+  hosts = lib.pipe ./. [
+    builtins.readDir
+    (lib.filterAttrs (name: _: name != "default.nix"))
+    lib.attrNames
   ];
 in
+  lib.genAttrs hosts (host:
+    lib.nixosSystem {
+      modules = [
+        self.inputs.preservation.nixosModules.default
+        self.inputs.sops-nix.nixosModules.default
+        self.outputs.nixosModules.default
+        {
+          _module.args.flake = self;
 
-lib.pipe ./. [
-  builtins.readDir
+          sops.defaultSopsFile = ./${host}/secrets.yaml;
 
-  (lib.filterAttrs (name: _: name != "default.nix"))
-
-  (lib.mapAttrs' (name: _:
-    let
-      hostName = lib.removeSuffix ".nix" name;
-      defaultSopsFile = ./${name}/secrets.yaml;
-    in
-    {
-      name = hostName;
-      value = lib.nixosSystem {
-        modules = inputModules ++ [
-          self.outputs.nixosModules.default
-          {
-            # Allow modules to refer to this flake by argument.
-            # WARN: Do not use `self` to import modules!
-            _module.args.flake = self;
-
-            sops.defaultSopsFile =
-              lib.mkIf
-                (builtins.pathExists defaultSopsFile)
-                defaultSopsFile;
-
-            networking = { inherit hostName; };
-          }
-
-          ../profiles/common.nix
-          ./${name}
-        ];
-      };
-    }))
-]
+          networking.hostName = lib.removeSuffix ".nix" host;
+        }
+        ../profiles/common.nix
+        ./${host}
+      ];
+    }
+  )
