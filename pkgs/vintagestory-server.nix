e2743e5a3a6e593e3767c409ba12609c75c45a13
Gediminas Valys <steamykins@gmail.com>
2026-04-25 11:35:08 +0300

    pkgs/vintagestory-server: 1.21.6 -> 1.22.0

--- pkgs/vintagestory-server.nix
+++ pkgs/vintagestory-server.nix
@@ -3,23 +3,23 @@
   stdenvNoCC,
   fetchurl,
   makeWrapper,
-  dotnet-runtime_8,
+  dotnet-runtime_10,
 }:
 
 let
   arm64Src = fetchurl {
-    url = "https://github.com/anegostudios/VintagestoryServerArm64/releases/download/1.21.0/vs_server_linux-arm64_1.21.0.tar.gz";
-    hash = "sha256-oPJrOZlBBVnx+isPPCcDZ/9Z1aKJ2ZBBnV8+MdGvWdE=";
+    url = "https://github.com/anegostudios/VintagestoryServerArm64/releases/download/1.22.0/vs_server_linux-arm64_1.22.0.tar.gz";
+    hash = "sha256-GRx/OliomkeEPnq40fsW1R8wMPemzq1jnHxIqQdnm/o=";
   };
 in
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "vintagestory-server";
-  version = "1.21.6";
+  version = "1.22.0";
 
   src = fetchurl {
     url = "https://cdn.vintagestory.at/gamefiles/stable/vs_server_linux-x64_${finalAttrs.version}.tar.gz";
-    hash = "sha256-Zk1Gj44mLJVB6JBIukXohKuzm6Jlmwld3h75VxIkfqw=";
+    hash = "sha256-cskh1uJLtHhYgclNcUm/OmJXpKsno3/DjWKRA1Aqxqs=";
   };
 
   sourceRoot = ".";
@@ -44,7 +44,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   '';
 
   preFixup = ''
-    makeWrapper ${lib.meta.getExe dotnet-runtime_8} $out/bin/vintagestory-server \
+    makeWrapper ${lib.meta.getExe dotnet-runtime_10} $out/bin/vintagestory-server \
       --add-flags $out/share/vintagestory/VintagestoryServer.dll
 
     find "$out/share/vintagestory/assets/" -not -path "*/fonts/*" -regex ".*/.*[A-Z].*" | while read -r file; do
