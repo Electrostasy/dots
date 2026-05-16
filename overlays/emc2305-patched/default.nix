f7bd792fce5d13f708e01c3e0bfb450124b7f71f
Gediminas Valys <steamykins@gmail.com>
2026-05-16 13:19:47 +0300

    overlays/emc2305-patched: update patches
    Fixes per-fan independent PWM setting, which was previously broken due
    to pwm_separate defaulting to false on devicetree and cooling devices
    not being registered with the thermal subsystem.

--- overlays/emc2305-patched/default.nix
+++ overlays/emc2305-patched/default.nix
@@ -15,12 +15,24 @@ final: prev: {
         nativeBuildInputs = kernel.moduleBuildDependencies;
 
         patches = [
-          # pwm_separate will default to false unless we go the platform data
-          # route, so fans will have combined PWM which is a _highly
-          # undesirable_ default. This patch makes pwm_separate default to
-          # true. Should probably be upstreamed because the mainline driver is
-          # unusable for >1 fans without it.
-          ./0003-hwmon-emc2305-separate-pwm-fix.patch
+          (final.fetchpatch {
+            name = "0001-hwmon-emc2305-Simplify-with-scoped-for-each-OF-child-loop.patch";
+            url = "https://patchwork.kernel.org/series/1036367/mbox/";
+            hash = "sha256-PdRrNQRBCtaFKjbVzZ+D5ktztV5TIZkl9ovIUtvwI4A=";
+          })
+          (final.fetchpatch {
+            name = "0002-hwmon-emc2305-Support-configurable-fan-PWM-at-shutdown.patch";
+            url = "https://patchwork.kernel.org/series/1087257/mbox/";
+            hash = "sha256-9xaxnFnZ4nwQ39HlF1oKgGcXq0OQWzgveglDHD2uU8Y=";
+          })
+
+          # pwm_separate will always be false on devicetree unless platform
+          # data is used, resulting in fans having combined PWM by default.
+          # Platform data is not used anywhere, therefore pwm_separate is
+          # practically broken according to upstream:
+          # https://patchwork.kernel.org/comment/26922589/
+          ./0003-hwmon-emc2305-Remove-platform-data.patch
+          ./0004-hwmon-emc2305-Remove-pwm_separate.patch
         ];
 
         postPatch = ''
