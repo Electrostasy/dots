6176ca88963199492d6bd621d57d97f85a9ed9e3
Gediminas Valys <steamykins@gmail.com>
2026-05-29 13:06:10 +0300

    modules/command-not-found: refactor to switch/case statements

--- modules/command-not-found.nix
+++ modules/command-not-found.nix
@@ -12,7 +12,7 @@ in
       interactive shells showing which Nix package (if any) provides a missing
       command.
 
-      Requires either nix-channels to be set and downloaded (sudo nix-channels
+      Requires either nix-channels to be set and downloaded (nix-channels
       --update) or the nixpkgs flake input set to a lockable tarball
       (https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz)'' // { default = true; };
 
@@ -32,32 +32,33 @@ in
       command_not_found_handle() {
         if [[ ! -e '${cfg.dbPath}' ]]; then
           echo '${cfg.dbPath} is missing!'
-          echo 'Please set programs.command-not-found.dbPath in your NixOS configuration to where programs.sqlite is.'
+          echo 'Set programs.command-not-found.dbPath in your NixOS configuration to where programs.sqlite is.'
           echo 'You can disable this feature by setting programs.command-not-found.enable to false in your NixOS configuration.'
           return 127
         fi
 
-        mapfile -t packages < <(${lib.getExe pkgs.sqlite} '${cfg.dbPath}' "SELECT DISTINCT package FROM Programs WHERE name = '$1';")
-        amount=''${#packages[@]}
-
         output="The program '$1' is not in your PATH. "
+        mapfile -t packages < <(${lib.getExe pkgs.sqlite} '${cfg.dbPath}' "SELECT DISTINCT package FROM Programs WHERE name = '$1';")
 
-        if [[ $amount -eq 0 ]]; then
-          output+='It is not provided by any indexed packages.'
-        else
-          maybe_one_of=
-          if [[ $amount -eq 1 ]]; then
+        case ''${#packages[@]} in
+          0)
+            output+='It is not provided by any indexed packages.'
+            printf '%b' "$output"
+            return 127
+            ;;
+          1)
             output+='It is provided by one package.\n'
-          else
+            ;;
+          *)
             output+='It is provided by several packages.\n'
             maybe_one_of=' one of'
-          fi
+            ;;
+        esac
 
-          output+="You can make it available in an ephemeral shell by typing$maybe_one_of the following:\n"
-          output+="$(printf '  nix shell nixpkgs#%s\n' "''${packages[@]}")\n\n"
-          output+="You can run it once by typing$maybe_one_of the following:\n"
-          output+="$(printf '  nix run nixpkgs#%s\n' "''${packages[@]}")"
-        fi
+        output+="You can make it available in an ephemeral shell by typing$maybe_one_of the following:\n"
+        output+="$(printf '    nix shell nixpkgs#%s\n' "''${packages[@]}")\n\n"
+        output+="You can run it once by typing$maybe_one_of the following:\n"
+        output+="$(printf '    nix run nixpkgs#%s\n' "''${packages[@]}")"
 
         printf '%b' "$output"
       }
@@ -73,32 +74,31 @@ in
       function fish_command_not_found
         if not test -e ${cfg.dbPath}
           echo '${cfg.dbPath} is missing!'
-          echo 'Please set programs.command-not-found.dbPath in your NixOS configuration to where programs.sqlite is.'
+          echo 'Set programs.command-not-found.dbPath in your NixOS configuration to where programs.sqlite is.'
           echo 'You can disable this feature by setting programs.command-not-found.enable to false in your NixOS configuration.'
           return 127
         end
 
-        set -l packages (${lib.getExe pkgs.sqlite} '${cfg.dbPath}' "SELECT DISTINCT package FROM Programs WHERE name = '$argv[1]';")
-        set -l amount (count $packages)
-
         set -l output "The program '$(set_color $fish_color_error; echo -n $argv[1]; set_color normal)' is not in your PATH. "
+        set -l packages (${lib.getExe pkgs.sqlite} '${cfg.dbPath}' "SELECT DISTINCT package FROM Programs WHERE name = '$argv[1]';")
 
-        if test $amount -eq 0
-          set -a output 'It is not provided by any indexed packages.\n'
-        else
-          if test $amount -eq 1
+        switch (count $packages)
+          case 0
+            set -a output 'It is not provided by any indexed packages.\n'
+            printf '%b' $output
+            return 127
+          case 1
             set -a output 'It is provided by one package.\n'
-          else
+          case '*'
             set -a output 'It is provided by several packages.\n'
             set -l maybe_one_of ' one of'
-          end
-
-          set -a output \
-            "You can make it available in an ephemeral shell by typing$maybe_one_of the following:\n" \
-            "$(printf '  nix shell nixpkgs#%s\n' $packages | fish_indent --ansi --only-unindent)" '\n\n' \
-            "You can run it once by typing$maybe_one_of the following:\n" \
-            "$(printf '  nix run nixpkgs#%s\n' $packages | fish_indent --ansi --only-unindent)" '\n'
         end
+
+        set -a output \
+          "You can make it available in an ephemeral shell by typing$maybe_one_of the following:\n" \
+          "$(printf '    nix shell nixpkgs#%s\n' $packages | fish_indent --ansi --only-unindent)" '\n\n' \
+          "You can run it once by typing$maybe_one_of the following:\n" \
+          "$(printf '    nix run nixpkgs#%s\n' $packages | fish_indent --ansi --only-unindent)" '\n'
 
         printf '%b' $output
       end
