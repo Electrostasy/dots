0bc315324dddd2d34d526a5fb1a47af71db2ebcf
Gediminas Valys <steamykins@gmail.com>
2026-08-03 12:45:18 +0300

    pkgs/ubootSige7: move env variables into env

--- pkgs/ubootSige7/package.nix
+++ pkgs/ubootSige7/package.nix
@@ -1,6 +1,7 @@
-{ buildUBoot
-, armTrustedFirmwareRK3588
-, rkbin
+{
+  buildUBoot,
+  armTrustedFirmwareRK3588,
+  rkbin,
 }:
 
 buildUBoot {
@@ -13,7 +14,13 @@ buildUBoot {
 
   defconfig = "sige7-rk3588_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
-  BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
-  ROCKCHIP_TPL = rkbin.TPL_RK3588;
-  filesToInstall = [ "u-boot.itb" "idbloader.img" "u-boot-rockchip.bin" ];
+  env = {
+    BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
+    ROCKCHIP_TPL = rkbin.TPL_RK3588;
+  };
+  filesToInstall = [
+    "u-boot.itb"
+    "idbloader.img"
+    "u-boot-rockchip.bin"
+  ];
 }
