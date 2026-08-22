f86a4be9f38faae17e8ec7f8766403807b1548d0
Gediminas Valys <steamykins@gmail.com>
2026-08-22 09:08:16 +0300

    Revert "hosts/terra: fix pcsx2 build failure"
    This reverts commit 5905135628b4ac85365656ada887a0cc666e0a6a.

--- hosts/terra/gaming.nix
+++ hosts/terra/gaming.nix
@@ -44,7 +44,7 @@
       dualsensectl
       mangohud
       mumble
-      (pcsx2.override { ffmpeg = pkgs.ffmpeg_8; }) # https://github.com/NixOS/nixpkgs/pull/552272
+      pcsx2
       umu-launcher
     ];
   };
